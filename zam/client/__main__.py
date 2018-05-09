636eacfd7c0f529fd462bb89c938f1f82597ed8d
David Larlet <david@larlet.fr>
2018-05-09 16:01:21 -0400

    Keep the master index at the root of the folder
    Add the date to the output folder name for branch based generations

--- zam/client/__main__.py
+++ zam/client/__main__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import datetime
 from pathlib import Path
 
 from logbook import StreamHandler
@@ -26,11 +27,17 @@ def generate(input_dir: str, output_dir: str, limit: int=None) -> None:
     articles = enhance_articles(source['list'], input_path, limit)
     html_output = render(title=source['libelle'], articles=articles)
     output_root_path = Path(output_dir)
-    current_branch = os.popen('git symbolic-ref --short HEAD')
-    output_path = output_root_path / current_branch.read().strip()
-    if not output_path.exists():
-        output_path.mkdir()
-    with open(output_path / 'index.html', 'w') as output_file:
+    current_branch = os.popen('git symbolic-ref --short HEAD').read().strip()
+    if current_branch == 'master':
+        output_filename = 'index.html'
+    else:
+        now = datetime.utcnow()
+        output_dir = f'{now.isoformat()[:10]}-{current_branch}'
+        output_path = output_root_path / output_dir
+        if not output_path.exists():
+            output_path.mkdir()
+        output_filename = output_path / 'index.html'
+    with open(output_filename, 'w') as output_file:
         output_file.write(html_output)
 
 

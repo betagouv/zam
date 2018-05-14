53175adc2c662153f8be79276aa157add1c02e9a
David Larlet <david@larlet.fr>
2018-05-13 23:51:13 -0400

    Reduce the size of the produced HTML
    From 2.5Mb to 1.2Mb so far by mutualizing articles detail infos.

--- zam/client/templates/__init__.py
+++ zam/client/templates/__init__.py
@@ -1,6 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader('.'),
+                  # Strip down as much as possible the size of the HTML
+                  # by avoiding extra white spaces everywhere.
+                  trim_blocks=True, lstrip_blocks=True)
 
 
 def render(**kwargs: dict) -> str:

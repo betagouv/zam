4d48db6bf4973fef4d7c498022dd908925bf8fe8
Ronan Amicel <ronan.amicel@gmail.com>
2018-06-05 17:40:49 +0200

    Fix packaging

--- aspirateur/setup.py
+++ aspirateur/setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="zam-aspirateur",
     version="0.1.0",
     url="https://github.com/betagouv/zam",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
     entry_points={"console_scripts": ["zam-aspirateur = zam_aspirateur.__main__:main"]},
     install_requires=["bleach", "dataclasses", "openpyxl", "requests"],
 )

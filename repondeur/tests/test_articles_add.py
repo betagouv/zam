19d61b6ab757ad5be8f18d488868175df6fa0985
David Larlet <david@larlet.fr>
2018-06-22 15:47:29 -0400

    Deal with dynamic Senat textes contents fetching

--- repondeur/tests/test_articles_add.py
+++ repondeur/tests/test_articles_add.py
@@ -89,3 +89,50 @@ def test_post_form_seance(app, dummy_lecture, dummy_amendements):
 
     amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
     assert amendement.subdiv_contenu["001"].startswith("Le code des relations entre")
+
+
+@responses.activate
+def test_post_form_senat(app, dummy_lecture, dummy_amendements):
+    from zam_repondeur.models import DBSession, Amendement, Lecture
+
+    with transaction.manager:
+        lecture = Lecture.get(
+            chambre=dummy_lecture[0],
+            session=dummy_lecture[1],
+            num_texte=dummy_lecture[2],
+        )
+        lecture.chambre = "senat"
+        lecture.session = "2017-2018"
+        lecture.num_texte = 63
+        DBSession.add(lecture)
+
+        amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+        amendement.chambre = "senat"
+        amendement.session = "2017-2018"
+        amendement.num_texte = 63
+        amendement.subdiv_num = 1
+        DBSession.add(amendement)
+
+    responses.add(
+        responses.GET,
+        "http://www.senat.fr/leg/pjl17-063.html",
+        body=(Path(__file__).parent / "sample_data" / "pjl17-063.html").read_text(
+            "utf-8", "ignore"
+        ),
+        status=200,
+    )
+
+    form = app.get("/lectures/senat/2017-2018/63/").form
+
+    resp = form.submit()
+
+    assert resp.status_code == 302
+    assert resp.location == "http://localhost/lectures/senat/2017-2018/63/"
+
+    resp = resp.follow()
+
+    assert resp.status_code == 200
+    assert "Articles récupérés" in resp.text
+
+    amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+    assert amendement.subdiv_contenu["001"].startswith("Au titre de l'exercice 2016")
--- repondeur/zam_repondeur/fetch.py
+++ repondeur/zam_repondeur/fetch.py
@@ -38,7 +38,9 @@ def get_articles(lecture: Lecture) -> None:
         else:
             url = f"{BASE_URL}{lecture.session}/projets/pl{lecture.num_texte:04}.asp"
     else:
-        url = f"http://www.senat.fr/leg/pjl17-{lecture.num_texte:03}.html"
+        url = (
+            f"http://www.senat.fr/leg/pjl{lecture.session[2:4]}-{lecture.num_texte:03}.html"
+        )
     items = parse(url)
     for article_content in items:
         if "alineas" in article_content and article_content["alineas"]:

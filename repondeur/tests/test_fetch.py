8c572ffc954c8f186255f612204ccec8ff807d60
David Larlet <david@larlet.fr>
2018-06-28 10:39:55 -0400

    Articles contents refactoring with unit tests

---
+++ repondeur/tests/test_fetch.py
@@ -0,0 +1,151 @@
+from pathlib import Path
+import transaction
+
+import pytest
+import responses
+
+
+@responses.activate
+def test_get_articles(app, dummy_lecture, dummy_amendements):
+    from zam_repondeur.fetch import get_articles
+    from zam_repondeur.models import DBSession, Amendement, Lecture
+
+    responses.add(
+        responses.GET,
+        "https://www.senat.fr/leg/pjl17-063.html",
+        body=(Path(__file__).parent / "sample_data" / "pjl17-063.html").read_text(
+            "utf-8", "ignore"
+        ),
+        status=200,
+    )
+    with transaction.manager:
+        lecture = Lecture.get(
+            chambre=dummy_lecture[0],
+            session=dummy_lecture[1],
+            num_texte=dummy_lecture[2],
+        )
+        lecture.chambre = "senat"
+        lecture.session = "2017-2018"
+        lecture.num_texte = 63
+
+        amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+        amendement.chambre = "senat"
+        amendement.session = "2017-2018"
+        amendement.num_texte = 63
+
+        get_articles(lecture)
+
+    amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+    assert amendement.subdiv_contenu["001"].startswith("Au titre de l'exercice 2016")
+
+
+@responses.activate
+def test_get_articles_with_mult(app, dummy_lecture, dummy_amendements):
+    from zam_repondeur.fetch import get_articles
+    from zam_repondeur.models import DBSession, Amendement, Lecture
+
+    responses.add(
+        responses.GET,
+        "https://www.senat.fr/leg/pjl17-063.html",
+        body=(Path(__file__).parent / "sample_data" / "pjl17-063.html").read_text(
+            "utf-8", "ignore"
+        ),
+        status=200,
+    )
+    with transaction.manager:
+        lecture = Lecture.get(
+            chambre=dummy_lecture[0],
+            session=dummy_lecture[1],
+            num_texte=dummy_lecture[2],
+        )
+        lecture.chambre = "senat"
+        lecture.session = "2017-2018"
+        lecture.num_texte = 63
+
+        amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+        amendement.chambre = "senat"
+        amendement.session = "2017-2018"
+        amendement.num_texte = 63
+        amendement.subdiv_num = "4"
+        amendement.subdiv_mult = "bis"
+
+        get_articles(lecture)
+
+    amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+    assert amendement.subdiv_contenu["001"].startswith("Ne donnent pas lieu à")
+
+
+def test_get_section_title():
+    from zam_repondeur.fetch import get_section_title
+
+    items = [
+        {
+            "id": "P1",
+            "titre": "Dispositions relatives à l'exercice 2016",
+            "type": "section",
+            "type_section": "partie",
+        },
+        {
+            "alineas": {},
+            "order": 1,
+            "section": "P1",
+            "statut": "none",
+            "titre": "1er",
+            "type": "article",
+        },
+    ]
+    article = {
+        "alineas": {},
+        "order": 1,
+        "section": "P1",
+        "statut": "none",
+        "titre": "1er",
+        "type": "article",
+    }
+    title = get_section_title(items, article)
+    assert title == "Dispositions relatives à l'exercice 2016"
+
+
+def test_get_section_title_unknown_reference():
+    from zam_repondeur.fetch import get_section_title
+
+    items = [
+        {
+            "id": "P1",
+            "titre": "Dispositions relatives à l'exercice 2016",
+            "type": "section",
+            "type_section": "partie",
+        },
+        {
+            "alineas": {},
+            "order": 1,
+            "section": "P1",
+            "statut": "none",
+            "titre": "1er",
+            "type": "article",
+        },
+    ]
+    article = {
+        "alineas": {},
+        "order": 1,
+        "section": "FOO",
+        "statut": "none",
+        "titre": "1er",
+        "type": "article",
+    }
+    assert get_section_title(items, article) == ""
+
+
+@pytest.mark.parametrize(
+    "input,num,mult",
+    [
+        ("2", "2", ""),
+        ("1er", "1", ""),
+        ("3 bis", "3", "bis"),
+        ("5 ter AAA", "5", "ter AAA"),
+    ],
+)
+def test_get_article_num_mult(input, num, mult):
+    from zam_repondeur.fetch import get_article_num_mult
+
+    assert get_article_num_mult({"titre": input}) == (num, mult)

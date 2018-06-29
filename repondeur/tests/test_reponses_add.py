5bc6574e92777c6c51262e16f4d69e027f769c63
David Larlet <david@larlet.fr>
2018-06-29 10:30:21 -0400

    Guess delimiter for CSV upload of reponses

--- repondeur/tests/test_reponses_add.py
+++ repondeur/tests/test_reponses_add.py
@@ -51,6 +51,37 @@ def test_post_form(app, dummy_lecture, dummy_amendements):
     assert amendement.position == 2
 
 
+def test_post_form_semicolumns(app, dummy_lecture, dummy_amendements):
+    from zam_repondeur.models import DBSession, Amendement
+
+    form = app.get("/lectures/an/15/269/amendements/list").form
+    path = Path(__file__).parent / "sample_data" / "reponses_semicolumns.csv"
+    form["reponses"] = Upload("file.csv", path.read_bytes())
+
+    resp = form.submit()
+
+    assert resp.status_code == 302
+    assert resp.location == "http://localhost/lectures/an/15/269/amendements/list"
+
+    resp = resp.follow()
+
+    assert resp.status_code == 200
+    assert "2 réponses chargées" in resp.text
+
+    amendement = DBSession.query(Amendement).filter(Amendement.num == 666).first()
+    assert amendement.avis == "Défavorable"
+    assert amendement.position == 1
+    assert "<strong>ipsum</strong>" in amendement.observations
+    assert "<blink>amet</blink>" not in amendement.observations
+
+    assert "<i>tempor</i>" in amendement.reponse
+    assert "<u>aliqua</u>" not in amendement.reponse
+
+    amendement = DBSession.query(Amendement).filter(Amendement.num == 999).first()
+    assert amendement.observations.startswith("Lorem")
+    assert amendement.position == 2
+
+
 def test_post_form_reponse_no_file(app, dummy_lecture, dummy_amendements):
 
     form = app.get("/lectures/an/15/269/amendements/list").form

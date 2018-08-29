f1aa9cab1e85d34153e52419397305c18866d88d
Ronan Amicel <ronan.amicel@gmail.com>
2018-08-29 10:56:25 +0200

    Add reporting of errors to Rollbar for async tasks

--- repondeur/zam_repondeur/scripts/worker.py
+++ repondeur/zam_repondeur/scripts/worker.py
@@ -1,8 +1,11 @@
+import logging
 import os
 import sys
-from typing import List
+from typing import Dict, List
 
+import rollbar
 from pyramid.paster import get_appsettings, setup_logging
+from rollbar.logger import RollbarHandler
 from sqlalchemy import engine_from_config
 
 from zam_repondeur.data import init_repository
@@ -20,8 +23,15 @@ def main(argv: List[str] = sys.argv) -> None:
     if len(argv) != 2:
         usage(argv)
     config_uri = argv[1]
+
     setup_logging(config_uri)
+
     settings = get_appsettings(config_uri)
+
+    rollbar_settings = extract_settings(settings, prefix="rollbar.")
+    if "access_token" in rollbar_settings and "environment" in rollbar_settings:
+        setup_rollbar_error_reporting(rollbar_settings)
+
     engine = engine_from_config(settings, "sqlalchemy.")
     DBSession.configure(bind=engine)
 
@@ -34,3 +44,23 @@ def main(argv: List[str] = sys.argv) -> None:
 
     consumer = huey.create_consumer(worker_type="thread", workers=1, max_delay=5.0)
     consumer.run()
+
+
+def extract_settings(settings: Dict[str, str], prefix: str) -> Dict[str, str]:
+    prefix_length = len(prefix)
+    return {
+        key[prefix_length:]: settings[key] for key in settings if key.startswith(prefix)
+    }
+
+
+def setup_rollbar_error_reporting(rollbar_settings: Dict[str, str]) -> None:
+    """
+    All log messages with level ERROR or higher will be sent to Rollbar
+    """
+    rollbar.init(**rollbar_settings)
+
+    rollbar_handler = RollbarHandler()
+    rollbar_handler.setLevel(logging.ERROR)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(rollbar_handler)

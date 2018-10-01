680752237ca75731efbc8878758fde4b1e9e4f61
Ronan Amicel <ronan.amicel@gmail.com>
2018-10-01 14:37:18 +0200

    Fix broken link to self on articles list page

--- repondeur/tests/test_articles_list.py
+++ repondeur/tests/test_articles_list.py
@@ -10,3 +10,9 @@ class TestListArticles:
         assert resp.status_code == 200
         titles = [node.text().strip() for node in resp.parser.css(".article")]
         assert titles == ["Chapitre Ier"]
+
+    def test_link_to_articles_list(self, app, lecture_an):
+        resp = app.get("http://localhost/lectures/an.15.269.PO717460/articles/")
+        assert resp.status_code == 200
+        link = resp.parser.css_first("nav.main .list").attributes.get("href")
+        assert link == "http://localhost/lectures/an.15.269.PO717460/articles/"

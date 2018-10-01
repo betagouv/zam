7d1ff30255f9e68e4f3840aa261d2772ee89bc7d
Ronan Amicel <ronan.amicel@gmail.com>
2018-10-01 15:18:25 +0200

    Fix inconsistent spelling of "après" in database
    The parsing of new amendements was fixed in https://github.com/betagouv/zam/pull/137
    but this retroactively fixes old values in the DB.

---
+++ repondeur/db_migrations/versions/4bec591c8da6_fix_inconsistent_spelling_of_apres.py
@@ -0,0 +1,23 @@
+"""Fix inconsistent spelling of apres
+
+Revision ID: 4bec591c8da6
+Revises: 7373c17f2bf1
+Create Date: 2018-10-01 12:54:59.114979
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "4bec591c8da6"
+down_revision = "7373c17f2bf1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("UPDATE articles SET pos='après' WHERE pos='apres';")
+
+
+def downgrade():
+    pass

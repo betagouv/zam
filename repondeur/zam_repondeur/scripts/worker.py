65e6ab4acb7f1cfd4ae704e776be52da7610b1b0
Ronan Amicel <ronan.amicel@gmail.com>
2018-10-19 17:28:58 +0200

    Flush all stale Huey locks on worker startup
    Huey can only flush static locks (those used in a decorator) by himself,
    not dynamic locks (those used in context managers) so we have to dig into
    the Redis data structure to remove them ourselves.
    
    cf. https://github.com/coleifer/huey/issues/308#issuecomment-379142489

--- repondeur/zam_repondeur/scripts/worker.py
+++ repondeur/zam_repondeur/scripts/worker.py
@@ -4,7 +4,9 @@ import sys
 from typing import Dict, List
 
 import rollbar
+from huey import RedisHuey
 from pyramid.paster import get_appsettings, setup_logging
+from redis.exceptions import ConnectionError
 from rollbar.logger import RollbarHandler
 from sqlalchemy import engine_from_config
 
@@ -12,6 +14,9 @@ from zam_repondeur import BASE_SETTINGS
 from zam_repondeur.data import init_repository
 from zam_repondeur.models import DBSession
 from zam_repondeur.tasks.huey import init_huey
+
+
+logger = logging.getLogger(__name__)
 
 
 def usage(argv: List[str]) -> None:
@@ -44,6 +49,12 @@ def main(argv: List[str] = sys.argv) -> None:
     from zam_repondeur.tasks.fetch import fetch_articles, fetch_amendements  # noqa
     from zam_repondeur.tasks.periodic import update_data, fetch_all_amendements  # noqa
 
+    try:
+        flush_stale_locks(huey)
+    except ConnectionError:
+        logger.exception("Failed to connect to Redis")
+        sys.exit(1)
+
     consumer = huey.create_consumer(
         worker_type="thread",
         workers=int(settings["huey.workers"]),
@@ -71,3 +82,26 @@ def setup_rollbar_error_reporting(rollbar_settings: Dict[str, str]) -> None:
 
     root_logger = logging.getLogger()
     root_logger.addHandler(rollbar_handler)
+
+
+def flush_stale_locks(huey: RedisHuey) -> None:
+    """
+    Flush stale Huey locks
+
+    Huey can only flush static locks (those used in a decorator) by himself,
+    not dynamic locks (those used in context managers) so we have to dig into
+    the Redis data structure to remove them ourselves.
+
+    cf. https://github.com/coleifer/huey/issues/308#issuecomment-379142489
+    """
+    redis_conn = huey.storage.conn
+    results = redis_conn.hgetall("huey.results.huey")
+    locks = [key for key in results.keys() if key.startswith(b"huey.lock.")]
+    if locks:
+        logger.info("Found stale locks: %r", locks)
+        response = redis_conn.hdel("huey.results.huey", *locks)
+        if response == len(locks):
+            logger.info("All locks removed successfully")
+        else:
+            logger.warning("Unexpected result from Redis: %r", response)
+    return

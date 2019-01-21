a4d8dbd581ff33020e7207bd432d9a2ba1ee4b54
Ronan Amicel <ronan.amicel@gmail.com>
2019-01-21 11:02:25 +0000

    Refactor: introduce Source class hierarchy

--- repondeur/zam_repondeur/fetch/amendements.py
+++ repondeur/zam_repondeur/fetch/amendements.py
@@ -1,17 +1,47 @@
-from typing import List, Tuple
+from typing import List, NamedTuple
 
 from zam_repondeur.fetch.an.amendements import aspire_an
 from zam_repondeur.fetch.senat.amendements import aspire_senat
 from zam_repondeur.models import Amendement, Lecture
 
 
-def get_amendements(lecture: Lecture) -> Tuple[List[Amendement], int, List[str]]:
-    title: str
-    if lecture.chambre == "senat":
-        amendements, created = aspire_senat(lecture=lecture)
-        return amendements, created, []  # Not pertinent in that case (unique file).
-    elif lecture.chambre == "an":
-        amendements, created, errored = aspire_an(lecture=lecture)
-        return amendements, created, errored
-    else:
+class Source:
+    pass
+
+
+class FetchResult(NamedTuple):
+    amendements: List[Amendement]
+    created: int
+    errored: List[str]
+
+
+class RemoteSource(Source):
+
+    def fetch(self, lecture: Lecture) -> FetchResult:
         raise NotImplementedError
+
+    @classmethod
+    def get_remote_source_for_chambre(cls, chambre: str) -> "RemoteSource":
+        if chambre == "an":
+            return AssembleeNationale()
+        elif chambre == "senat":
+            return Senat()
+        else:
+            raise NotImplementedError
+
+
+class AssembleeNationale(RemoteSource):
+    def fetch(self, lecture: Lecture) -> FetchResult:
+        amendements, created, errored = aspire_an(lecture=lecture)
+        return FetchResult(amendements, created, errored)
+
+
+class Senat(RemoteSource):
+    def fetch(self, lecture: Lecture) -> FetchResult:
+        amendements, created = aspire_senat(lecture=lecture)
+        return FetchResult(amendements, created, [])
+
+
+def get_amendements(lecture: Lecture) -> FetchResult:
+    source = RemoteSource.get_remote_source_for_chambre(lecture.chambre)
+    return source.fetch(lecture)

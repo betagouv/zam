b8d84200530a4c8fb82e3d5afeb17d6250d7d10f
David Larlet <david@larlet.fr>
2019-02-15 21:13:18 -0500

    Deal with auth related errors
    It appears that a required field is not enough.
    
    See https://rollbar.com/zam/zam/items/177/ and https://rollbar.com/zam/zam/items/178/

--- repondeur/tests/views/test_auth.py
+++ repondeur/tests/views/test_auth.py
@@ -37,6 +37,15 @@ def test_user_gets_an_auth_cookie_after_identifying_herself(app):
         assert cookie.get_nonstandard_attr("SameSite") == "Lax"
 
 
+def test_user_must_authenticate_with_an_email(app):
+    resp = app.post("/identification", {"email": ""})
+
+    assert resp.status_code == 302
+    assert resp.location == "https://zam.test/identification"
+    resp = resp.follow()
+    assert "La saisie d’une adresse de courriel est requise." in resp.text
+
+
 def test_user_loses_the_auth_cookie_when_loggin_out(app):
     app.post("/identification", {"email": "jane.doe@example.com"})
     assert "auth_tkt" in app.cookies  # the auth cookie is set
@@ -59,7 +68,7 @@ def test_authentified_user_is_not_redirected_to_login_page(app):
     assert resp.status_code == 200
 
 
-def test_new_user_can_enter_their_name_on_the_welcome_page(app):
+def test_new_user_must_enter_their_name_on_the_welcome_page(app):
     from zam_repondeur.models import DBSession, User
 
     user = DBSession.query(User).filter_by(email="jane.doe@example.com").first()
@@ -83,6 +92,37 @@ def test_new_user_can_enter_their_name_on_the_welcome_page(app):
 
     user = DBSession.query(User).filter_by(email="jane.doe@example.com").first()
     assert user.name == "Something Else"
+
+
+def test_new_user_without_name_get_an_error(app):
+    from zam_repondeur.models import DBSession, User
+
+    user = DBSession.query(User).filter_by(email="jane.doe@example.com").first()
+    assert user is None
+
+    resp = app.post("/identification", {"email": "jane.doe@example.com"})
+    assert resp.status_code == 302
+    assert (
+        resp.location
+        == "https://zam.test/bienvenue?source=https%3A%2F%2Fzam.test%2Flectures%2F"
+    )
+
+    user = DBSession.query(User).filter_by(email="jane.doe@example.com").first()
+    assert user.name is None
+
+    resp = resp.follow()
+    assert resp.form["name"].value == "Jane Doe"  # prefilled based on email
+
+    resp.form["name"] = ""
+    resp = resp.form.submit()
+
+    assert resp.status_code == 302
+    assert resp.location == "https://zam.test/bienvenue"
+    resp = resp.follow()
+    assert "La saisie d’un nom est requise." in resp.text
+
+    user = DBSession.query(User).filter_by(email="jane.doe@example.com").first()
+    assert user.name is None
 
 
 def test_user_with_a_name_skips_the_welcome_page(app, user_david):

2cb819ff49b7f69b213bf32ff9787f20463c12ea
Ronan Amicel <ronan.amicel@gmail.com>
2019-03-19 19:29:12 +0000

    Yet another refactoring of the HTML word diff

--- repondeur/zam_repondeur/models/events/helpers.py
+++ repondeur/zam_repondeur/models/events/helpers.py
@@ -1,34 +1,48 @@
-from collections import namedtuple
 from difflib import Differ
+from functools import reduce
 from html import escape
 from itertools import groupby
 from operator import attrgetter
-from typing import List
+from typing import NamedTuple
 
 
 differ = Differ()
-Diff = namedtuple("Diff", ["code", "word"])
+
 
 CODES_TO_TAGS = {"+ ": "ins", "- ": "del", "  ": None}
+
+
+class Delta(NamedTuple):
+    code: str
+    text: str
+
+    @classmethod
+    def create(cls, delta_string: str) -> "Delta":
+        return cls(code=delta_string[:2], text=delta_string[2:])
+
+    def merge(self: "Delta", other: "Delta") -> "Delta":
+        if not isinstance(other, Delta):
+            raise TypeError
+        if self.code != other.code:
+            raise ValueError
+        return Delta(self.code, self.text + " " + other.text)
+
+    def to_html(self) -> str:
+        tag = CODES_TO_TAGS[self.code]
+        text = escape(self.text)
+        if tag:
+            return f"<{tag}>{text}</{tag}>"
+        return text
 
 
 def html_diff(old_text: str, new_text: str) -> str:
     old_words = old_text.split()
     new_words = new_text.split()
-    deltas = (
-        Diff(delta[:2], delta[2:]) for delta in differ.compare(old_words, new_words)
-    )
-    html_fragments = (
-        wrap_with_tag(code, [diff.word for diff in diffs])
-        for code, diffs in groupby(deltas, key=attrgetter("code"))
+    word_deltas = (Delta.create(s) for s in differ.compare(old_words, new_words))
+    merged_deltas = (
+        reduce(Delta.merge, word_deltas)
+        for code, word_deltas in groupby(word_deltas, key=attrgetter("code"))
         if code != "? "
     )
+    html_fragments = (delta.to_html() for delta in merged_deltas)
     return " ".join(html_fragments).strip()
-
-
-def wrap_with_tag(code: str, words: List[str]) -> str:
-    tag = CODES_TO_TAGS[code]
-    text = escape(" ".join(words))
-    if tag:
-        return f"<{tag}>{text}</{tag}>"
-    return text

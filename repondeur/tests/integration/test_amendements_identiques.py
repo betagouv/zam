d4802fe9170f1de529e444fc355e440de39e258c
Ronan Amicel <ronan.amicel@gmail.com>
2019-03-27 17:43:02 +0100

    Do not mark abandoned amendements as identiques

--- repondeur/tests/integration/test_amendements_identiques.py
+++ repondeur/tests/integration/test_amendements_identiques.py
@@ -31,13 +31,24 @@ def test_amendements_identiques_with_abandoned(
 ):
     from zam_repondeur.models import DBSession
 
-    LECTURE_URL = f"{wsgi_server.application_url}lectures/{lecture_an.url_key}"
     with transaction.manager:
+        DBSession.add_all(amendements_an)
+
         amendements_an[0].id_identique = 42
         amendements_an[1].id_identique = 42
+        assert amendements_an[0].all_identiques == [amendements_an[1]]
+        assert amendements_an[1].all_identiques == [amendements_an[0]]
+
         amendements_an[1].sort = "retiré"
-        DBSession.add_all(amendements_an)
-        driver.get(f"{LECTURE_URL}/amendements")
+        assert amendements_an[1].is_abandoned
+        assert amendements_an[0].all_identiques == []
+        assert amendements_an[1].all_identiques == [amendements_an[0]]
+
+    LECTURE_URL = f"{wsgi_server.application_url}lectures/{lecture_an.url_key}"
+    driver.get(f"{LECTURE_URL}/amendements")
+
+    amendements = driver.find_elements_by_css_selector("tbody tr")
+    assert len(amendements) == 2
 
     identiques = driver.find_elements_by_css_selector("tbody tr td.identique")
     assert len(identiques) == 0

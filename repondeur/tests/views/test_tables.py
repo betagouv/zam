9578ec177ed4a25f90d3063df33eb8c0dadaa421
Ronan Amicel <ronan.amicel@gmail.com>
2019-04-02 17:34:41 +0200

    Refactor tests: move amendements setup to module-level fixtures

--- repondeur/tests/views/test_tables.py
+++ repondeur/tests/views/test_tables.py
@@ -28,8 +28,44 @@ def user_ronan(user_ronan):
     return user_ronan
 
 
-def test_tables_empty(app, lecture_an, user_david):
+@pytest.fixture
+def david_has_no_amendement(user_david, lecture_an, user_david_table_an):
+    from zam_repondeur.models import DBSession
 
+    with transaction.manager:
+        DBSession.add(user_david_table_an)
+
+    assert len(user_david.table_for(lecture_an).amendements) == 0
+
+
+@pytest.fixture
+def david_has_one_amendement(
+    user_david, lecture_an, user_david_table_an, amendements_an
+):
+    from zam_repondeur.models import DBSession
+
+    with transaction.manager:
+        DBSession.add(user_david_table_an)
+        user_david_table_an.amendements.append(amendements_an[0])
+
+    assert len(user_david.table_for(lecture_an).amendements) == 1
+
+
+@pytest.fixture
+def david_has_two_amendements(
+    user_david, lecture_an, user_david_table_an, amendements_an
+):
+    from zam_repondeur.models import DBSession
+
+    with transaction.manager:
+        DBSession.add(user_david_table_an)
+        user_david_table_an.amendements.append(amendements_an[0])
+        user_david_table_an.amendements.append(amendements_an[1])
+
+    assert len(user_david.table_for(lecture_an).amendements) == 2
+
+
+def test_tables_empty(app, lecture_an, user_david):
     email = user_david.email
     resp = app.get(f"/lectures/an.15.269.PO717460/tables/{email}", user=email)
 
@@ -38,14 +74,8 @@ def test_tables_empty(app, lecture_an, user_david):
 
 
 def test_tables_with_amendement(
-    app, lecture_an, amendements_an, user_david, user_david_table_an
+    app, lecture_an, amendements_an, user_david, david_has_one_amendement
 ):
-    from zam_repondeur.models import DBSession
-
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-
     email = user_david.email
     resp = app.get(f"/lectures/an.15.269.PO717460/tables/{email}", user=email)
 
@@ -55,10 +85,10 @@ def test_tables_with_amendement(
     assert f'Nº&nbsp;<span class="numero">{amendements_an[1]}</span>' not in resp.text
 
 
-def test_tables_grab_amendement(app, lecture_an, amendements_an, user_david):
+def test_tables_grab_amendement(
+    app, lecture_an, amendements_an, user_david, david_has_no_amendement
+):
     from zam_repondeur.models import DBSession, User
-
-    assert len(user_david.table_for(lecture_an).amendements) == 0
 
     email = user_david.email
     resp = app.post(
@@ -66,10 +96,12 @@ def test_tables_grab_amendement(app, lecture_an, amendements_an, user_david):
         {"nums": [amendements_an[0].num], "submit-table": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
+
     user_david = DBSession.query(User).filter(User.email == user_david.email).first()
     table = user_david.table_for(lecture_an)
     assert len(table.amendements) == 1
@@ -82,10 +114,10 @@ def test_tables_grab_amendement(app, lecture_an, amendements_an, user_david):
     )
 
 
-def test_tables_grab_amendements(app, lecture_an, amendements_an, user_david):
+def test_tables_grab_amendements(
+    app, lecture_an, amendements_an, user_david, david_has_no_amendement
+):
     from zam_repondeur.models import DBSession, User
-
-    assert len(user_david.table_for(lecture_an).amendements) == 0
 
     email = user_david.email
     resp = app.post(
@@ -93,6 +125,7 @@ def test_tables_grab_amendements(app, lecture_an, amendements_an, user_david):
         {"nums": [amendements_an[0].num, amendements_an[1].num], "submit-table": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
@@ -117,15 +150,9 @@ def test_tables_grab_amendements(app, lecture_an, amendements_an, user_david):
 
 
 def test_tables_release_amendement(
-    app, lecture_an, amendements_an, user_david, user_david_table_an
+    app, lecture_an, amendements_an, user_david, david_has_one_amendement
 ):
     from zam_repondeur.models import DBSession, Amendement, User
-
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-
-    assert len(user_david.table_for(lecture_an).amendements) == 1
 
     email = user_david.email
     resp = app.post(
@@ -133,10 +160,12 @@ def test_tables_release_amendement(
         {"nums": [amendements_an[0].num], "submit-index": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
+
     user_david = DBSession.query(User).filter(User.email == user_david.email).first()
     table = user_david.table_for(lecture_an)
     assert len(table.amendements) == 0
@@ -153,16 +182,9 @@ def test_tables_release_amendement(
 
 
 def test_tables_release_amendements(
-    app, lecture_an, amendements_an, user_david, user_david_table_an
+    app, lecture_an, amendements_an, user_david, david_has_two_amendements
 ):
     from zam_repondeur.models import DBSession, Amendement, User
-
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-        user_david_table_an.amendements.append(amendements_an[1])
-
-    assert len(user_david.table_for(lecture_an).amendements) == 2
 
     email = user_david.email
     resp = app.post(
@@ -170,6 +192,7 @@ def test_tables_release_amendements(
         {"nums": [amendements_an[0].num, amendements_an[1].num], "submit-index": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
@@ -209,14 +232,13 @@ class TestTransfer:
         user_david_table_an,
         user_ronan,
         user_ronan_table_an,
+        david_has_one_amendement,
     ):
         from zam_repondeur.models import DBSession, User
 
         with transaction.manager:
-            DBSession.add_all([user_david_table_an, user_ronan_table_an])
-            user_david_table_an.amendements.append(amendements_an[0])
+            DBSession.add(user_ronan_table_an)
 
-        assert len(user_david.table_for(lecture_an).amendements) == 1
         assert len(user_ronan.table_for(lecture_an).amendements) == 0
 
         email = user_david.email
@@ -225,11 +247,13 @@ class TestTransfer:
             {"nums": [amendements_an[0].num], "target": user_ronan.email},
             user=email,
         )
+
         assert resp.status_code == 302
         assert (
             resp.location
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
+
         user_david = (
             DBSession.query(User).filter(User.email == user_david.email).first()
         )
@@ -249,15 +273,9 @@ class TestTransfer:
         )
 
     def test_transfer_one_amendement_to_myself_is_a_no_op(
-        self, app, lecture_an, amendements_an, user_david, user_david_table_an
+        self, app, lecture_an, amendements_an, user_david, david_has_one_amendement
     ):
         from zam_repondeur.models import DBSession, User
-
-        with transaction.manager:
-            DBSession.add(user_david_table_an)
-            user_david_table_an.amendements.append(amendements_an[0])
-
-        assert len(user_david.table_for(lecture_an).amendements) == 1
 
         email = user_david.email
         resp = app.post(
@@ -265,11 +283,13 @@ class TestTransfer:
             {"nums": [amendements_an[0].num], "submit-table": True},
             user=email,
         )
+
         assert resp.status_code == 302
         assert (
             resp.location
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
+
         user_david = (
             DBSession.query(User).filter(User.email == user_david.email).first()
         )
@@ -280,15 +300,9 @@ class TestTransfer:
         assert len(table_david.amendements[0].events) == 0
 
     def test_transfer_one_amendement_to_index_manually_is_forbidden(
-        self, app, lecture_an, amendements_an, user_david, user_david_table_an
+        self, app, lecture_an, amendements_an, user_david, david_has_one_amendement
     ):
         from zam_repondeur.models import DBSession, User
-
-        with transaction.manager:
-            DBSession.add(user_david_table_an)
-            user_david_table_an.amendements.append(amendements_an[0])
-
-        assert len(user_david.table_for(lecture_an).amendements) == 1
 
         email = user_david.email
         resp = app.post(
@@ -296,10 +310,12 @@ class TestTransfer:
             {"nums": [amendements_an[0].num], "target": ""},
             user=email,
         )
+
         assert resp.status_code == 302
         assert resp.location == (
             "https://zam.test/lectures/an.15.269.PO717460/transfer_amendements?nums=666"
         )
+
         user_david = (
             DBSession.query(User).filter(User.email == user_david.email).first()
         )
@@ -315,17 +331,11 @@ class TestTransfer:
         lecture_an,
         amendements_an,
         user_david,
-        user_david_table_an,
+        david_has_two_amendements,
         user_ronan,
     ):
         from zam_repondeur.models import DBSession, User
 
-        with transaction.manager:
-            DBSession.add(user_david_table_an)
-            user_david_table_an.amendements.append(amendements_an[0])
-            user_david_table_an.amendements.append(amendements_an[1])
-
-        assert len(user_david.table_for(lecture_an).amendements) == 2
         assert len(user_ronan.table_for(lecture_an).amendements) == 0
 
         email = user_david.email
@@ -337,11 +347,13 @@ class TestTransfer:
             },
             user=email,
         )
+
         assert resp.status_code == 302
         assert (
             resp.location
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
+
         user_david = (
             DBSession.query(User).filter(User.email == user_david.email).first()
         )
@@ -369,15 +381,10 @@ class TestTransfer:
 
 
 def test_tables_steal_amendement(
-    app, lecture_an, amendements_an, user_david, user_david_table_an, user_ronan
+    app, lecture_an, amendements_an, user_david, david_has_one_amendement, user_ronan
 ):
     from zam_repondeur.models import DBSession, User
 
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-
-    assert len(user_david.table_for(lecture_an).amendements) == 1
     assert len(user_ronan.table_for(lecture_an).amendements) == 0
 
     email = user_ronan.email
@@ -386,10 +393,12 @@ def test_tables_steal_amendement(
         {"nums": [amendements_an[0].num], "submit-table": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
+
     user_david = DBSession.query(User).filter(User.email == user_david.email).first()
     table_david = user_david.table_for(lecture_an)
     user_ronan = DBSession.query(User).filter(User.email == user_ronan.email).first()
@@ -406,16 +415,10 @@ def test_tables_steal_amendement(
 
 
 def test_tables_steal_amendements(
-    app, lecture_an, amendements_an, user_david, user_david_table_an, user_ronan
+    app, lecture_an, amendements_an, user_david, david_has_two_amendements, user_ronan
 ):
     from zam_repondeur.models import DBSession, User
 
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-        user_david_table_an.amendements.append(amendements_an[1])
-
-    assert len(user_david.table_for(lecture_an).amendements) == 2
     assert len(user_ronan.table_for(lecture_an).amendements) == 0
 
     email = user_ronan.email
@@ -424,10 +427,12 @@ def test_tables_steal_amendements(
         {"nums": [amendements_an[0].num, amendements_an[1].num], "submit-table": True},
         user=email,
     )
+
     assert resp.status_code == 302
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
+
     user_david = DBSession.query(User).filter(User.email == user_david.email).first()
     table_david = user_david.table_for(lecture_an)
     user_ronan = DBSession.query(User).filter(User.email == user_ronan.email).first()
@@ -455,21 +460,15 @@ def test_tables_steal_amendements(
     [["", {"updated": "666_999"}], ["666", {"updated": "666_999"}], ["666_999", {}]],
 )
 def test_tables_check_with_amendements(
-    app, lecture_an, amendements_an, user_david, user_david_table_an, current, updated
+    app, user_david, david_has_two_amendements, current, updated
 ):
-    from zam_repondeur.models import DBSession
-
-    with transaction.manager:
-        DBSession.add(user_david_table_an)
-        user_david_table_an.amendements.append(amendements_an[0])
-        user_david_table_an.amendements.append(amendements_an[1])
-
     email = user_david.email
     resp = app.get(
         f"/lectures/an.15.269.PO717460/tables/{email}/check",
         {"current": current},
         user=email,
     )
+
     assert resp.status_code == 200
     assert resp.json == updated
 
@@ -487,5 +486,6 @@ def test_tables_check_without_amendements(
         {"current": current},
         user=email,
     )
+
     assert resp.status_code == 200
     assert resp.json == updated

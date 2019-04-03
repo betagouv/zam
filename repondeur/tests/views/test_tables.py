5574dacfa4057fe106be76ea30d642bf1458d37f
Ronan Amicel <ronan.amicel@gmail.com>
2019-04-03 12:53:39 +0200

    Refactor tests: extract assertion helpers + reload before checking for changes

--- repondeur/tests/views/test_tables.py
+++ repondeur/tests/views/test_tables.py
@@ -65,6 +65,37 @@ def david_has_two_amendements(
     assert len(user_david.table_for(lecture_an).amendements) == 2
 
 
+def assert_amendements_are_on_table_for(amendements, user):
+    for amendement in amendements:
+        assert amendement.user_table.user.email == user.email
+
+
+def assert_amendement_is_on_table_for(amendement, user):
+    assert_amendements_are_on_table_for([amendement], user)
+
+
+def assert_amendements_are_on_the_index(amendements):
+    for amendement in amendements:
+        assert amendement.user_table is None
+
+
+def assert_amendement_is_on_the_index(amendement):
+    assert_amendements_are_on_the_index([amendement])
+
+
+def assert_amendement_has_event_with_summary(amendement, summary):
+    events = amendement.events
+    assert len(events) == 1
+
+    event = events[0]
+    assert event.render_summary() == summary
+
+
+def assert_amendements_have_event_with_summary(amendements, summary):
+    for amendement in amendements:
+        assert_amendement_has_event_with_summary(amendement, summary)
+
+
 def test_tables_empty(app, lecture_an, user_david):
     email = user_david.email
     resp = app.get(f"/lectures/an.15.269.PO717460/tables/{email}", user=email)
@@ -88,7 +119,7 @@ def test_tables_with_amendement(
 def test_tables_grab_amendement(
     app, lecture_an, amendements_an, user_david, david_has_no_amendement
 ):
-    from zam_repondeur.models import DBSession, User
+    from zam_repondeur.models import Amendement
 
     email = user_david.email
     resp = app.post(
@@ -102,22 +133,20 @@ def test_tables_grab_amendement(
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
 
-    user_david = DBSession.query(User).filter(User.email == user_david.email).first()
-    table = user_david.table_for(lecture_an)
-    assert len(table.amendements) == 1
-    assert table.amendements[0].num == amendements_an[0].num
-    assert table.amendements[0].lecture == amendements_an[0].lecture
-    assert len(table.amendements[0].events) == 1
-    assert table.amendements[0].events[0].render_summary() == (
-        "<abbr title='david@example.com'>David</abbr> "
-        "a mis l’amendement sur sa table"
+    # Reload amendement as it was updated in another transaction
+    amendement = Amendement.get(lecture_an, amendements_an[0].num)
+
+    assert_amendement_is_on_table_for(amendement, user_david)
+    assert_amendement_has_event_with_summary(
+        amendement,
+        "<abbr title='david@example.com'>David</abbr> a mis l’amendement sur sa table",
     )
 
 
 def test_tables_grab_amendements(
     app, lecture_an, amendements_an, user_david, david_has_no_amendement
 ):
-    from zam_repondeur.models import DBSession, User
+    from zam_repondeur.models import Amendement
 
     email = user_david.email
     resp = app.post(
@@ -130,31 +159,26 @@ def test_tables_grab_amendements(
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
-    user_david = DBSession.query(User).filter(User.email == user_david.email).first()
-    table = user_david.table_for(lecture_an)
-    assert len(table.amendements) == 2
-    assert table.amendements[0].num == amendements_an[0].num
-    assert table.amendements[0].lecture == amendements_an[0].lecture
-    assert len(table.amendements[0].events) == 1
-    assert table.amendements[0].events[0].render_summary() == (
-        "<abbr title='david@example.com'>David</abbr> "
-        "a mis l’amendement sur sa table"
-    )
-    assert table.amendements[1].num == amendements_an[1].num
-    assert table.amendements[1].lecture == amendements_an[1].lecture
-    assert len(table.amendements[1].events) == 1
-    assert table.amendements[1].events[0].render_summary() == (
-        "<abbr title='david@example.com'>David</abbr> "
-        "a mis l’amendement sur sa table"
+
+    # Reload amendements as they were updated in another transaction
+    amendements = [Amendement.get(lecture_an, amdt.num) for amdt in amendements_an]
+
+    assert_amendements_are_on_table_for(amendements, user_david)
+    assert_amendements_have_event_with_summary(
+        amendements,
+        "<abbr title='david@example.com'>David</abbr> a mis l’amendement sur sa table",
     )
 
 
 def test_tables_release_amendement(
     app, lecture_an, amendements_an, user_david, david_has_one_amendement
 ):
-    from zam_repondeur.models import DBSession, Amendement, User
+    from zam_repondeur.models import Amendement
 
     email = user_david.email
+
+    assert_amendement_is_on_table_for(amendements_an[0], user_david)
+
     resp = app.post(
         f"/lectures/an.15.269.PO717460/tables/{email}",
         {"nums": [amendements_an[0].num], "submit-index": True},
@@ -166,25 +190,21 @@ def test_tables_release_amendement(
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
 
-    user_david = DBSession.query(User).filter(User.email == user_david.email).first()
-    table = user_david.table_for(lecture_an)
-    assert len(table.amendements) == 0
-    amendement = (
-        DBSession.query(Amendement)
-        .filter(Amendement.num == amendements_an[0].num)
-        .first()
-    )
-    assert len(amendement.events) == 1
-    assert amendement.events[0].render_summary() == (
+    # Reload amendement as it was updated in another transaction
+    amendement = Amendement.get(lecture_an, amendements_an[0].num)
+
+    assert_amendement_is_on_the_index(amendement)
+    assert_amendement_has_event_with_summary(
+        amendement,
         "<abbr title='david@example.com'>David</abbr> "
-        "a remis l’amendement dans l’index"
+        "a remis l’amendement dans l’index",
     )
 
 
 def test_tables_release_amendements(
     app, lecture_an, amendements_an, user_david, david_has_two_amendements
 ):
-    from zam_repondeur.models import DBSession, Amendement, User
+    from zam_repondeur.models import Amendement
 
     email = user_david.email
     resp = app.post(
@@ -197,28 +217,15 @@ def test_tables_release_amendements(
     assert (
         resp.location == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
     )
-    user_david = DBSession.query(User).filter(User.email == user_david.email).first()
-    table = user_david.table_for(lecture_an)
-    assert len(table.amendements) == 0
-    amendement = (
-        DBSession.query(Amendement)
-        .filter(Amendement.num == amendements_an[0].num)
-        .first()
-    )
-    assert len(amendement.events) == 1
-    assert amendement.events[0].render_summary() == (
+
+    # Reload amendements as they were updated in another transaction
+    amendements = [Amendement.get(lecture_an, amdt.num) for amdt in amendements_an]
+
+    assert_amendements_are_on_the_index(amendements)
+    assert_amendements_have_event_with_summary(
+        amendements,
         "<abbr title='david@example.com'>David</abbr> "
-        "a remis l’amendement dans l’index"
-    )
-    amendement = (
-        DBSession.query(Amendement)
-        .filter(Amendement.num == amendements_an[1].num)
-        .first()
-    )
-    assert len(amendement.events) == 1
-    assert amendement.events[0].render_summary() == (
-        "<abbr title='david@example.com'>David</abbr> "
-        "a remis l’amendement dans l’index"
+        "a remis l’amendement dans l’index",
     )
 
 
@@ -234,7 +241,7 @@ class TestTransfer:
         user_ronan_table_an,
         david_has_one_amendement,
     ):
-        from zam_repondeur.models import DBSession, User
+        from zam_repondeur.models import DBSession, Amendement
 
         with transaction.manager:
             DBSession.add(user_ronan_table_an)
@@ -254,28 +261,20 @@ class TestTransfer:
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
 
-        user_david = (
-            DBSession.query(User).filter(User.email == user_david.email).first()
-        )
-        table_david = user_david.table_for(lecture_an)
-        user_ronan = (
-            DBSession.query(User).filter(User.email == user_ronan.email).first()
-        )
-        table_ronan = user_ronan.table_for(lecture_an)
-        assert len(table_david.amendements) == 0
-        assert len(table_ronan.amendements) == 1
-        assert table_ronan.amendements[0].num == amendements_an[0].num
-        assert table_ronan.amendements[0].lecture == amendements_an[0].lecture
-        assert len(table_ronan.amendements[0].events) == 1
-        assert table_ronan.amendements[0].events[0].render_summary() == (
+        # Reload amendement as it was updated in another transaction
+        amendement = Amendement.get(lecture_an, amendements_an[0].num)
+
+        assert_amendement_is_on_table_for(amendement, user_ronan)
+        assert_amendement_has_event_with_summary(
+            amendement,
             "<abbr title='david@example.com'>David</abbr> "
-            "a transféré l’amendement à « Ronan (ronan@example.com) »"
+            "a transféré l’amendement à « Ronan (ronan@example.com) »",
         )
 
     def test_transfer_one_amendement_to_myself_is_a_no_op(
         self, app, lecture_an, amendements_an, user_david, david_has_one_amendement
     ):
-        from zam_repondeur.models import DBSession, User
+        from zam_repondeur.models import Amendement
 
         email = user_david.email
         resp = app.post(
@@ -290,19 +289,16 @@ class TestTransfer:
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
 
-        user_david = (
-            DBSession.query(User).filter(User.email == user_david.email).first()
-        )
-        table_david = user_david.table_for(lecture_an)
-        assert len(table_david.amendements) == 1
-        assert table_david.amendements[0].num == amendements_an[0].num
-        assert table_david.amendements[0].lecture == amendements_an[0].lecture
-        assert len(table_david.amendements[0].events) == 0
+        # Reload amendement before checking for changes in the web transaction
+        amendement = Amendement.get(lecture_an, amendements_an[0].num)
+
+        assert_amendement_is_on_table_for(amendement, user_david)
+        assert len(amendement.events) == 0
 
     def test_transfer_one_amendement_to_index_manually_is_forbidden(
         self, app, lecture_an, amendements_an, user_david, david_has_one_amendement
     ):
-        from zam_repondeur.models import DBSession, User
+        from zam_repondeur.models import Amendement
 
         email = user_david.email
         resp = app.post(
@@ -316,14 +312,11 @@ class TestTransfer:
             "https://zam.test/lectures/an.15.269.PO717460/transfer_amendements?nums=666"
         )
 
-        user_david = (
-            DBSession.query(User).filter(User.email == user_david.email).first()
-        )
-        table_david = user_david.table_for(lecture_an)
-        assert len(table_david.amendements) == 1
-        assert table_david.amendements[0].num == amendements_an[0].num
-        assert table_david.amendements[0].lecture == amendements_an[0].lecture
-        assert len(table_david.amendements[0].events) == 0
+        # Reload amendement before checking for changes in the web transaction
+        amendement = Amendement.get(lecture_an, amendements_an[0].num)
+
+        assert_amendement_is_on_table_for(amendement, user_david)
+        assert len(amendement.events) == 0
 
     def test_transfer_multiple_amendements_to_someone_else(
         self,
@@ -334,7 +327,8 @@ class TestTransfer:
         david_has_two_amendements,
         user_ronan,
     ):
-        from zam_repondeur.models import DBSession, User
+        # from zam_repondeur.models import DBSession, User
+        from zam_repondeur.models import Amendement
 
         assert len(user_ronan.table_for(lecture_an).amendements) == 0
 
@@ -354,29 +348,13 @@ class TestTransfer:
             == f"https://zam.test/lectures/an.15.269.PO717460/tables/{email}"
         )
 
-        user_david = (
-            DBSession.query(User).filter(User.email == user_david.email).first()
-        )
-        table_david = user_david.table_for(lecture_an)
-        user_ronan = (
-            DBSession.query(User).filter(User.email == user_ronan.email).first()
-        )
-        table_ronan = user_ronan.table_for(lecture_an)
-        assert len(table_david.amendements) == 0
-        assert len(table_ronan.amendements) == 2
-        assert table_ronan.amendements[0].num == amendements_an[0].num
-        assert table_ronan.amendements[0].lecture == amendements_an[0].lecture
-        assert len(table_ronan.amendements[0].events) == 1
-        assert table_ronan.amendements[0].events[0].render_summary() == (
+        # Reload amendements as they were updated in another transaction
+        amendements = [Amendement.get(lecture_an, amdt.num) for amdt in amendements_an]
+        assert_amendements_are_on_table_for(amendements, user_ronan)
+        assert_amendements_have_event_with_summary(
+            amendements,
             "<abbr title='david@example.com'>David</abbr> "
-            "a transféré l’amendement à « Ronan (ronan@example.com) »"
-        )
-        assert table_ronan.amendements[1].num == amendements_an[1].num
-        assert table_ronan.amendements[1].lecture == amendements_an[1].lecture
-        assert len(table_ronan.amendements[1].events) == 1
-        assert table_ronan.amendements[1].events[0].render_summary() == (
-            "<abbr title='david@example.com'>David</abbr> "
-            "a transféré l’amendement à « Ronan (ronan@example.com) »"
+            "a transféré l’amendement à « Ronan (ronan@example.com) »",
         )
 
 

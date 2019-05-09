c1c547ecfb946629e05a25163c488a4ce7f5aa6b
David Larlet <david@larlet.fr>
2019-05-09 13:49:05 -0400

    Restore ability to export a JSON from a lecture

---
+++ repondeur/tests/test_export_json.py
@@ -0,0 +1,349 @@
+import transaction
+
+import ujson as json
+
+
+def test_write_json(
+    lecture_senat, article1_senat, article1av_senat, article7bis_senat, tmpdir
+):
+    from zam_repondeur.export.json import write_json
+    from zam_repondeur.models import Amendement
+
+    filename = str(tmpdir.join("test.json"))
+
+    with transaction.manager:
+        article1_senat.user_content.title = "Titre art. 1 Sénat"
+        article1_senat.user_content.presentation = "Présentation art. 1 Sénat"
+        amendement = Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=42,
+            rectif=1,
+            auteur="M. DUPONT",
+            groupe="RDSE",
+            matricule="000000",
+            corps="<p>L'article 1 est supprimé.</p>",
+            expose="<p>Cet article va à l'encontre du principe d'égalité.</p>",
+            resume="Suppression de l'article",
+            position=1,
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1av_senat,
+            alinea="",
+            num=57,
+            auteur="M. DURAND",
+            groupe="Les Républicains",
+            matricule="000001",
+            corps="baz",
+            expose="qux",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article7bis_senat,
+            alinea="",
+            num=21,
+            auteur="M. MARTIN",
+            groupe=None,
+            matricule="000002",
+            corps="quux",
+            expose="quuz",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=43,
+            auteur="M. JEAN",
+            groupe="Les Indépendants",
+            matricule="000003",
+            corps="corge",
+            expose="grault",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=596,
+            rectif=1,
+            parent=amendement,
+            auteur="M. JEAN",
+            groupe="Les Indépendants",
+            matricule="000003",
+            corps="corge",
+            expose="grault",
+        )
+        counter = write_json(lecture_senat, filename, request={})
+
+    with open(filename, "r", encoding="utf-8-sig") as f_:
+        backup = json.loads(f_.read())
+        amendements = backup["amendements"]
+        articles = backup["articles"]
+
+    assert counter["amendements"] == len(amendements) == 5
+    assert counter["articles"] == len(articles) == 3
+
+    assert amendements[0] == {
+        "alinea": "",
+        "auteur": "M. DUPONT",
+        "avis": "",
+        "chambre": "senat",
+        "comments": "",
+        "corps": "<p>L'article 1 est supprimé.</p>",
+        "date_depot": "",
+        "id_discussion_commune": "",
+        "expose": "<p>Cet article va à l'encontre du principe d'égalité.</p>",
+        "groupe": "RDSE",
+        "id_identique": "",
+        "matricule": "000000",
+        "num_texte": 63,
+        "article": "Article 1",
+        "article_titre": "Titre art. 1 Sénat",
+        "parent": "",
+        "num": 42,
+        "objet": "",
+        "organe": "PO78718",
+        "position": 1,
+        "rectif": 1,
+        "reponse": "",
+        "resume": "Suppression de l'article",
+        "session": "2017-2018",
+        "sort": "",
+        "gouvernemental": False,
+        "article_order": "6|001|01|__________|1",
+        "affectation_email": "",
+        "affectation_name": "",
+    }
+    assert [amdt["article_order"] for amdt in amendements] == [
+        "6|001|01|__________|1",
+        "6|007|02|__________|1",
+        "6|001|01|__________|1",
+        "6|001|01|__________|0",
+        "6|001|01|__________|1",
+    ]
+    assert articles == [
+        {"presentation": "", "sort_key_as_str": "6|001|01|__________|0", "title": ""},
+        {
+            "presentation": "Présentation art. 1 Sénat",
+            "sort_key_as_str": "6|001|01|__________|1",
+            "title": "Titre art. 1 Sénat",
+        },
+        {"presentation": "", "sort_key_as_str": "6|007|02|__________|1", "title": ""},
+    ]
+
+
+def test_write_json_full(lecture_senat, article1_senat, tmpdir):
+    from zam_repondeur.export.json import write_json
+    from zam_repondeur.models import Amendement
+
+    filename = str(tmpdir.join("test.json"))
+
+    with transaction.manager:
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=42,
+            rectif=1,
+            auteur="M. DUPONT",
+            groupe="RDSE",
+            matricule="000000",
+            corps="<p>L'article 1 est supprimé.</p>",
+            expose="<p>Cet article va à l'encontre du principe d'égalité.</p>",
+            resume="Suppression de l'article",
+            position=1,
+            avis="Défavorable",
+            objet="Un objet",
+            reponse="<p>La réponse</p>",
+            comments="<strong>Lisez-moi</strong>",
+        )
+        counter = write_json(lecture_senat, filename, request={})
+
+    with open(filename, "r", encoding="utf-8-sig") as f_:
+        backup = json.loads(f_.read())
+        amendements = backup["amendements"]
+        articles = backup["articles"]
+
+    assert counter["amendements"] == len(amendements) == 1
+    assert counter["articles"] == len(articles) == 1
+
+    assert amendements[0] == {
+        "alinea": "",
+        "auteur": "M. DUPONT",
+        "avis": "Défavorable",
+        "chambre": "senat",
+        "comments": "<strong>Lisez-moi</strong>",
+        "corps": "<p>L'article 1 est supprimé.</p>",
+        "date_depot": "",
+        "id_discussion_commune": "",
+        "expose": "<p>Cet article va à l'encontre du principe d'égalité.</p>",
+        "groupe": "RDSE",
+        "id_identique": "",
+        "matricule": "000000",
+        "num_texte": 63,
+        "article": "Article 1",
+        "article_titre": "",
+        "parent": "",
+        "num": 42,
+        "objet": "Un objet",
+        "organe": "PO78718",
+        "position": 1,
+        "rectif": 1,
+        "reponse": "<p>La réponse</p>",
+        "resume": "Suppression de l'article",
+        "session": "2017-2018",
+        "sort": "",
+        "gouvernemental": False,
+        "article_order": "6|001|01|__________|1",
+        "affectation_email": "",
+        "affectation_name": "",
+    }
+
+
+def test_write_with_affectation(
+    lecture_senat, article1_senat, tmpdir, user_david_table_senat
+):
+    from zam_repondeur.export.json import write_json
+    from zam_repondeur.models import Amendement, DBSession
+
+    filename = str(tmpdir.join("test.json"))
+
+    with transaction.manager:
+        DBSession.add(user_david_table_senat)
+        amendement = Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=42,
+            rectif=1,
+            auteur="M. DUPONT",
+            groupe="RDSE",
+            matricule="000000",
+            corps="<p>L'article 1 est supprimé.</p>",
+            expose="<p>Cet article va à l'encontre du principe d'égalité.</p>",
+        )
+        user_david_table_senat.amendements.append(amendement)
+
+        counter = write_json(lecture_senat, filename, request={})
+
+    with open(filename, "r", encoding="utf-8-sig") as f_:
+        backup = json.loads(f_.read())
+        amendements = backup["amendements"]
+        articles = backup["articles"]
+
+    assert counter["amendements"] == len(amendements) == 1
+    assert counter["articles"] == len(articles) == 1
+    assert amendements[0]["affectation_email"] == "david@example.com"
+    assert amendements[0]["affectation_name"] == "David"
+
+
+def test_write_json_sous_amendement(
+    lecture_senat, article1_senat, article1av_senat, article7bis_senat, tmpdir
+):
+    from zam_repondeur.export.json import write_json
+    from zam_repondeur.models import Amendement
+
+    filename = str(tmpdir.join("test.json"))
+
+    with transaction.manager:
+        amendement = Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=42,
+            rectif=1,
+            auteur="M. DUPONT",
+            groupe="RDSE",
+            matricule="000000",
+            corps="<p>L'article 1 est supprimé.</p>",
+            expose="<p>Cet article va à l'encontre du principe d'égalité.</p>",
+            resume="Suppression de l'article",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1av_senat,
+            alinea="",
+            num=57,
+            auteur="M. DURAND",
+            groupe="Les Républicains",
+            matricule="000001",
+            corps="baz",
+            expose="qux",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article7bis_senat,
+            alinea="",
+            num=21,
+            auteur="M. MARTIN",
+            groupe=None,
+            matricule="000002",
+            corps="quux",
+            expose="quuz",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=43,
+            auteur="M. JEAN",
+            groupe="Les Indépendants",
+            matricule="000003",
+            corps="corge",
+            expose="grault",
+        )
+        Amendement.create(
+            lecture=lecture_senat,
+            article=article1_senat,
+            alinea="",
+            num=596,
+            rectif=1,
+            parent=amendement,
+            auteur="M. JEAN",
+            groupe="Les Indépendants",
+            matricule="000003",
+            corps="corge",
+            expose="grault",
+        )
+        counter = write_json(lecture_senat, filename, request={})
+
+    with open(filename, "r", encoding="utf-8-sig") as f_:
+        backup = json.loads(f_.read())
+        amendements = backup["amendements"]
+        articles = backup["articles"]
+
+    assert counter["amendements"] == len(amendements) == 5
+    assert counter["articles"] == len(articles) == 3
+
+    assert amendements[-1] == {
+        "alinea": "",
+        "auteur": "M. JEAN",
+        "avis": "",
+        "article_order": "6|001|01|__________|1",
+        "chambre": "senat",
+        "comments": "",
+        "expose": "grault",
+        "date_depot": "",
+        "id_discussion_commune": "",
+        "corps": "corge",
+        "groupe": "Les Indépendants",
+        "id_identique": "",
+        "matricule": "000003",
+        "num_texte": 63,
+        "article": "Article 1",
+        "article_titre": "",
+        "parent": "42 rect.",
+        "num": 596,
+        "objet": "",
+        "organe": "PO78718",
+        "position": "",
+        "rectif": 1,
+        "reponse": "",
+        "resume": "",
+        "session": "2017-2018",
+        "sort": "",
+        "gouvernemental": False,
+        "affectation_email": "",
+        "affectation_name": "",
+    }
---
+++ repondeur/zam_repondeur/export/json.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+import ujson as json
+from pyramid.request import Request
+
+from zam_repondeur.models import Amendement, Lecture
+
+
+EXCLUDED_FIELDS = {"first_identique_num"}
+
+
+def write_json(lecture: Lecture, filename: str, request: Request) -> Counter:
+    counter = Counter({"amendements": 0, "articles": 0})
+    with open(filename, "w", encoding="utf-8-sig") as file_:
+        amendements = []
+        for amendement in sorted(lecture.amendements):
+            amendements.append(export_amendement_for_json(amendement))
+            counter["amendements"] += 1
+        articles = []
+        for article in sorted(lecture.articles):
+            articles.append(article.asdict())
+            counter["articles"] += 1
+        file_.write(
+            json.dumps({"amendements": amendements, "articles": articles}, indent=4)
+        )
+    return counter
+
+
+def export_amendement_for_json(amendement: Amendement) -> dict:
+    return {k: v for k, v in amendement.asdict().items() if k not in EXCLUDED_FIELDS}

1a64dab8d07e5808bbd028b3f6877fd6cd2c0621
Ronan Amicel <ronan.amicel@gmail.com>
2019-05-13 11:33:30 +0000

    Warn user when leaving amendement edit form in case of unsaved changes

---
+++ repondeur/zam_repondeur/static/js/unsaved.js
@@ -0,0 +1,27 @@
+class UnsavedChanges extends Stimulus.Controller {
+    formIsChanged(event) {
+        this.setChanged("true");
+    }
+
+    leaving(event) {
+        if (this.isFormChanged()) {
+            // Most browsers will show a default message, not our custom string :)
+            event.returnValue = "Si vous quittez cette page sans cliquer sur le bouton Enregistrer, vos modifications seront perdues. Êtes-vous sûr(e) de vouloir quitter cette page?";
+            return event.returnValue;
+        }
+    }
+
+    allowFormSubmission(event) {
+        this.setChanged("false");
+    }
+
+    setChanged(changed) {
+        this.data.set("changed", changed);
+    }
+
+    isFormChanged() {
+        return this.data.get("changed") == "true";
+    }
+}
+
+application.register('unsaved-changes', UnsavedChanges)

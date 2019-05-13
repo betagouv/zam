500cbe5ff0a40524b6fb8344aafa6f147e71b792
David Larlet <david@larlet.fr>
2019-05-13 14:07:24 +0000

    Rename methods for unsaved

--- repondeur/zam_repondeur/static/js/unsaved.js
+++ repondeur/zam_repondeur/static/js/unsaved.js
@@ -1,27 +1,25 @@
 class UnsavedChanges extends Stimulus.Controller {
-    formIsChanged(event) {
-        this.setChanged('true')
-    }
-
     leaving(event) {
-        if (this.isFormChanged()) {
+        if (this.isDirty()) {
             // Most browsers will show a default message, not our custom string :)
             event.returnValue =
-                'Si vous quittez cette page sans cliquer sur le bouton Enregistrer, vos modifications seront perdues. Êtes-vous sûr(e) de vouloir quitter cette page?'
+                'Si vous quittez cette page sans cliquer sur le bouton Enregistrer, ' +
+                'vos modifications seront perdues. ' +
+                'Êtes-vous sûr·e de vouloir quitter cette page ?'
             return event.returnValue
         }
     }
 
+    setDirty(event) {
+        this.data.set('dirty', 'true')
+    }
+
     allowFormSubmission(event) {
-        this.setChanged('false')
+        this.data.set('dirty', 'false')
     }
 
-    setChanged(changed) {
-        this.data.set('changed', changed)
-    }
-
-    isFormChanged() {
-        return this.data.get('changed') === 'true'
+    isDirty() {
+        return this.data.get('dirty') === 'true'
     }
 }
 

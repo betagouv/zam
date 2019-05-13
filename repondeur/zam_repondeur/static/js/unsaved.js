699837a08d9a0042330f994a05c45c6c83cbb09d
David Larlet <david@larlet.fr>
2019-05-13 13:42:20 +0000

    Apply JSPrettier

--- repondeur/zam_repondeur/static/js/unsaved.js
+++ repondeur/zam_repondeur/static/js/unsaved.js
@@ -1,26 +1,27 @@
 class UnsavedChanges extends Stimulus.Controller {
     formIsChanged(event) {
-        this.setChanged("true");
+        this.setChanged('true')
     }
 
     leaving(event) {
         if (this.isFormChanged()) {
             // Most browsers will show a default message, not our custom string :)
-            event.returnValue = "Si vous quittez cette page sans cliquer sur le bouton Enregistrer, vos modifications seront perdues. Êtes-vous sûr(e) de vouloir quitter cette page?";
-            return event.returnValue;
+            event.returnValue =
+                'Si vous quittez cette page sans cliquer sur le bouton Enregistrer, vos modifications seront perdues. Êtes-vous sûr(e) de vouloir quitter cette page?'
+            return event.returnValue
         }
     }
 
     allowFormSubmission(event) {
-        this.setChanged("false");
+        this.setChanged('false')
     }
 
     setChanged(changed) {
-        this.data.set("changed", changed);
+        this.data.set('changed', changed)
     }
 
     isFormChanged() {
-        return this.data.get("changed") == "true";
+        return this.data.get('changed') == 'true'
     }
 }
 

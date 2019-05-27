fb278406e5b8273ed2873c18057daf5bc25c6380
David Larlet <david@larlet.fr>
2019-05-27 17:53:56 -0400

    Make sure there is no orphan amendement in batch

--- repondeur/tests/test_lecture_batch.py
+++ repondeur/tests/test_lecture_batch.py
@@ -234,3 +234,100 @@ def test_lecture_post_batch_unset_amendement(
         "<abbr title='david@example.com'>David</abbr> "
         "a sorti l’amendement du lot dans lequel il était."
     )
+
+
+def test_lecture_post_batch_reset_amendement(
+    app,
+    lecture_an,
+    article1_an,
+    amendements_an,
+    user_david,
+    user_david_table_an,
+    david_has_two_amendements,
+):
+    from zam_repondeur.models import Amendement, DBSession
+
+    DBSession.add_all(amendements_an)
+    assert not amendements_an[0].batch
+    assert not amendements_an[1].batch
+
+    with transaction.manager:
+        amendement_777 = Amendement.create(
+            lecture=lecture_an, article=article1_an, num=777
+        )
+        user_david_table_an.amendements.append(amendement_777)
+        assert not amendement_777.batch
+
+    # First we associate two amendements
+    resp = app.get(
+        "/lectures/an.15.269.PO717460/batch_amendements",
+        {"nums": amendements_an},
+        user=user_david,
+    )
+    form = resp.form
+    resp = form.submit("submit-to")
+
+    # Reload amendement as it was updated in another transaction
+    amendement_666 = Amendement.get(lecture_an, amendements_an[0].num)
+    amendement_999 = Amendement.get(lecture_an, amendements_an[1].num)
+
+    # Both amendements are in the same batch
+    assert amendement_666.batch.pk == 1
+    assert amendement_999.batch.pk == 1
+    assert amendement_666.batch.amendements == [amendement_666, amendement_999]
+
+    # Then we re-associate two others (containing the first one)
+    resp = app.get(
+        "/lectures/an.15.269.PO717460/batch_amendements",
+        {"nums": [amendement_666, amendement_777]},
+        user=user_david,
+    )
+    form = resp.form
+    resp = form.submit("submit-to")
+
+    # We're redirected to our table
+    assert resp.status_code == 302
+    assert (
+        resp.location
+        == f"https://zam.test/lectures/an.15.269.PO717460/tables/{user_david.email}"
+    )
+
+    # Reload amendement as it was updated in another transaction.
+    amendement_666 = Amendement.get(lecture_an, amendements_an[0].num)
+    amendement_999 = Amendement.get(lecture_an, amendements_an[1].num)
+    amendement_777 = Amendement.get(lecture_an, amendement_777.num)
+
+    # A new batch is created and 999 has no batch anymore.
+    assert amendement_666.batch.pk == 2
+    assert not amendement_999.batch
+    assert amendement_777.batch.pk == 2
+    assert amendement_666.batch.amendements == [amendement_666, amendement_777]
+
+    # We should have events for all actions.
+    assert len(amendement_666.events) == 3
+    assert amendement_666.events[0].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a placé l’amendement dans un lot avec les amendements numéro 777."
+    )
+    assert amendement_666.events[1].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a sorti l’amendement du lot dans lequel il était."
+    )
+    assert amendement_666.events[2].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a placé l’amendement dans un lot avec les amendements numéro 999."
+    )
+    assert len(amendement_999.events) == 2
+    assert amendement_999.events[0].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a sorti l’amendement du lot dans lequel il était."
+    )
+    assert amendement_999.events[1].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a placé l’amendement dans un lot avec les amendements numéro 666."
+    )
+    assert len(amendement_777.events) == 1
+    assert amendement_777.events[0].render_summary() == (
+        "<abbr title='david@example.com'>David</abbr> "
+        "a placé l’amendement dans un lot avec les amendements numéro 666."
+    )

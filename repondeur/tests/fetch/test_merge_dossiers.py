5871b5f72b60e23a0a4bf6e8a07239e4ca6500ee
Ronan Amicel <ronan.amicel@beta.gouv.fr>
2019-06-04 19:20:15 +0200

    More tests (WIP)

--- repondeur/tests/fetch/test_merge_dossiers.py
+++ repondeur/tests/fetch/test_merge_dossiers.py
@@ -15,3 +15,36 @@ class TestMergeDossiers():
 
         dossiers1 = {"a": DossierRef("a", "titre1", [])}
         assert DossierRef.merge_dossiers({}, dossiers1) == dossiers1
+
+    def test_merge_different_dossiers(self):
+        from zam_repondeur.fetch.an.dossiers.models import DossierRef
+
+        dossiers1 = {"a": DossierRef("a", "titre1", [])}
+        dossiers2 = {"b": DossierRef("b", "titre2", [])}
+        assert DossierRef.merge_dossiers(dossiers1, dossiers2) == {
+            "a": DossierRef("a", "titre1", []),
+            "b": DossierRef("b", "titre2", []),
+        }
+
+    def test_merge_different_dossiers_reversed(self):
+        from zam_repondeur.fetch.an.dossiers.models import DossierRef
+
+        dossiers1 = {"a": DossierRef("a", "titre1", [])}
+        dossiers2 = {"b": DossierRef("b", "titre2", [])}
+        assert DossierRef.merge_dossiers(dossiers2, dossiers1) == {
+            "a": DossierRef("a", "titre1", []),
+            "b": DossierRef("b", "titre2", []),
+        }
+
+    def test_merge_same_dossier(self):
+        from zam_repondeur.fetch.an.dossiers.models import DossierRef, LectureRef
+
+        dossiers1 = {"a": DossierRef("a", "titre1", [
+            LectureRef(...),
+        ])}
+        dossiers2 = {"1": DossierRef("a", "titre2", [
+            LectureRef(...),
+        ])}
+        assert DossierRef.merge_dossiers(dossiers1, dossiers2) == {
+            "a": DossierRef("a", "titre1", [...]),
+        }

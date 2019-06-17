7e98eb9ee37e7ddad533eed01618dcbef7d3f204
David Larlet <david@larlet.fr>
2019-06-17 14:58:41 -0400

    Refactor get_one_or_create
    * query options are now applied even on retry, was only on first attempt
    * kwargs are only updated for creation, remains unchanged on subsequent get

--- repondeur/zam_repondeur/models/__init__.py
+++ repondeur/zam_repondeur/models/__init__.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Any, Tuple
 
 from sqlalchemy.exc import IntegrityError
@@ -19,26 +20,32 @@ from .events.article import *  # noqa
 from .events.lecture import *  # noqa
 
 
+def _get_one(model: Any, options: Any = None, **kwargs: Any) -> Tuple[Any, bool]:
+    query = DBSession.query(model).filter_by(**kwargs)
+    if options is not None:
+        query = query.options(options)
+    return query.one(), False
+
+
+def _create(model: Any, create_kwargs: Any = None, **kwargs: Any) -> Tuple[Any, bool]:
+    kwargs = deepcopy(kwargs)  # Otherwise updated in place for all subsequent queries.
+    kwargs.update(create_kwargs or {})
+    with DBSession.begin_nested():
+        created = model.create(**kwargs)
+        DBSession.add(created)
+    return created, True
+
+
 def get_one_or_create(
     model: Any, create_kwargs: Any = None, options: Any = None, **kwargs: Any
 ) -> Tuple[Any, bool]:
     try:
-        query = DBSession.query(model).filter_by(**kwargs)
-        if options is not None:
-            query = query.options(options)
-        return query.one(), False
+        return _get_one(model, options, **kwargs)
     except NoResultFound:
-        kwargs.update(create_kwargs or {})
         try:
-            with DBSession.begin_nested():
-                created = model.create(**kwargs)
-                DBSession.add(created)
-            return created, True
+            return _create(model, create_kwargs, **kwargs)
         except IntegrityError:  # Race condition.
             try:
-                return DBSession.query(model).filter_by(**kwargs).one(), False
+                return _get_one(model, options, **kwargs)
             except NoResultFound:  # Retry to raise the appropriated IntegrityError.
-                with DBSession.begin_nested():
-                    created = model.create(**kwargs)
-                    DBSession.add(created)
-                return created, True
+                return _create(model, create_kwargs, **kwargs)

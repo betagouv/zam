0b061c459e53f0afb29a6898a6297a5d8be96332
Ronan Amicel <ronan.amicel@beta.gouv.fr>
2019-06-17 14:21:02 +0000

    Add script to load data

---
+++ repondeur/zam_repondeur/scripts/load_data.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import sys
+from argparse import ArgumentParser, Namespace
+from typing import List
+
+from pyramid.paster import get_appsettings, setup_logging
+
+from zam_repondeur import BASE_SETTINGS
+from zam_repondeur.data import init_repository, repository
+
+
+logger = logging.getLogger(__name__)
+
+
+def usage(argv: List[str]) -> None:
+    cmd = os.path.basename(argv[0])
+    print("usage: %s <config_uri>\n" '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv: List[str] = sys.argv) -> None:
+
+    args = parse_args(argv[1:])
+
+    setup_logging(args.config_uri)
+
+    logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
+
+    settings = get_appsettings(args.config_uri)
+    settings = {**BASE_SETTINGS, **settings}
+
+    init_repository(settings)
+    repository.load_data()
+
+
+def parse_args(argv: List[str]) -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("config_uri")
+    return parser.parse_args(argv)

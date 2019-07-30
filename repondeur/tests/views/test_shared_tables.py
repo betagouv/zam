d4a4dac596e7dbf126a753f43d2a3ba09f190ee6
David Larlet <david@larlet.fr>
2019-07-30 20:35:30 +0000

    Move the delete button for shared table to detail

--- repondeur/tests/views/test_shared_tables.py
+++ repondeur/tests/views/test_shared_tables.py
@@ -69,6 +69,34 @@ def test_get_shared_tables_edit_form(
 
     assert resp.status_code == 200
     assert "Éditer cette boîte" in resp.text
+    assert "Supprimer" in resp.text
+    assert "button button-sm danger" in resp.text
+
+
+def test_get_shared_tables_edit_form_has_active_delete_link_if_no_amendement(
+    app, lecture_an, amendements_an, user_david, shared_table_lecture_an
+):
+    resp = app.get(f"/lectures/an.15.269.PO717460/boites/test-table/", user=user_david)
+
+    assert resp.status_code == 200
+    assert "Supprimer" in resp.text
+    assert "button button-sm danger" in resp.text
+
+
+def test_get_shared_tables_edit_form_has_disabled_delete_link_if_amendement(
+    app, lecture_an, amendements_an, user_david, shared_table_lecture_an
+):
+    from zam_repondeur.models import DBSession
+
+    with transaction.manager:
+        DBSession.add(shared_table_lecture_an)
+        shared_table_lecture_an.amendements.append(amendements_an[0])
+
+    resp = app.get(f"/lectures/an.15.269.PO717460/boites/test-table/", user=user_david)
+
+    assert resp.status_code == 200
+    assert "Supprimer" in resp.text
+    assert "button button-sm disabled" in resp.text
 
 
 def test_post_shared_tables_edit_form(

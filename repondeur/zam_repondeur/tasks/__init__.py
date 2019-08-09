8a43cbb6e37c9a2d3b2ff27188c77a893dabe35a
Ronan Amicel <ronan.amicel@beta.gouv.fr>
2019-08-09 19:45:14 +0000

    Store huey instance in the registry and expose it as a request property

--- repondeur/zam_repondeur/tasks/__init__.py
+++ repondeur/zam_repondeur/tasks/__init__.py
@@ -1,10 +1,23 @@
+from huey import Huey
 from pyramid.config import Configurator
+from pyramid.request import Request
+from zope.interface import Interface
 
 from .huey import init_huey
+
+
+class IHuey(Interface):
+    pass
 
 
 def includeme(config: Configurator) -> None:
     """
     Called automatically via config.include("zam_repondeur.tasks")
     """
-    init_huey(config.registry.settings)
+    huey = init_huey(config.registry.settings)
+    config.registry.registerUtility(component=huey, provided=IHuey)
+    config.add_request_method(get_huey, "huey", reify=True)
+
+
+def get_huey(request: Request) -> Huey:
+    return request.registry.queryUtility(IHuey)

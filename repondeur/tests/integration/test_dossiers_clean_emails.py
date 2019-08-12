272f15225610b0dea3ae0844f40631ec6849f005
David Larlet <david@larlet.fr>
2019-08-12 11:06:17 -0400

    Skip tests based on copy-paste if headless mode on

--- repondeur/tests/integration/test_dossiers_clean_emails.py
+++ repondeur/tests/integration/test_dossiers_clean_emails.py
@@ -55,6 +55,8 @@ from selenium.webdriver.common.keys import Keys
     ],
 )
 def test_dossier_paste_emails(wsgi_server, driver, dossier_an_url, initial, expected):
+    if driver.options().headless:
+        pytest.skip("Copy-pasting only available if browser not in headless mode.")
     driver.get(f"{dossier_an_url}/invite")
     emails_textarea = driver.find_element_by_css_selector('[name="emails"]')
     assert emails_textarea.is_displayed()

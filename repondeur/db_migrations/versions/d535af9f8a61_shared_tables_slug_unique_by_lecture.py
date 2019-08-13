26969bd97bb0a75f56ba6882f0b4cb74262fbf63
David Larlet <david@larlet.fr>
2019-08-13 17:24:27 -0400

    Shared tables slug should only be unique by lecture

---
+++ repondeur/db_migrations/versions/d535af9f8a61_shared_tables_slug_unique_by_lecture.py
@@ -0,0 +1,31 @@
+"""Shared tables slug unique by lecture
+
+Revision ID: d535af9f8a61
+Revises: 9022d8bc8ab9
+Create Date: 2019-08-13 17:21:42.064343
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "d535af9f8a61"
+down_revision = "9022d8bc8ab9"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_unique_constraint(
+        op.f("shared_tables_slug_lecture_pk_key"),
+        "shared_tables",
+        ["slug", "lecture_pk"],
+    )
+    op.drop_constraint("shared_tables_slug_key", "shared_tables", type_="unique")
+
+
+def downgrade():
+    op.create_unique_constraint("shared_tables_slug_key", "shared_tables", ["slug"])
+    op.drop_constraint(
+        op.f("shared_tables_slug_lecture_pk_key"), "shared_tables", type_="unique"
+    )

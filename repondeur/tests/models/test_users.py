857338207f6aa70f1382cdea56d161c626ab07e7
Ronan Amicel <ronan.amicel@beta.gouv.fr>
2019-09-03 08:55:52 +0000

    Refactor: group related tests in classes

--- repondeur/tests/models/test_users.py
+++ repondeur/tests/models/test_users.py
@@ -3,37 +3,36 @@ from datetime import datetime, timedelta
 from freezegun import freeze_time
 
 
-def test_user_activity_default(users_repository, user_david):
-    assert not user_david.is_active
-
-
-def test_user_activity_in_use(app, user_david, dossier_plfss2018):
-    app.get("/dossiers/plfss-2018/", user=user_david)
-    assert user_david.is_active
-
-
-def test_user_activity_not_anymore_in_use(app, user_david, dossier_plfss2018):
-    app.get("/dossiers/plfss-2018/", user=user_david)
-    assert user_david.is_active
-
-    with freeze_time(datetime.utcnow() + timedelta(minutes=29)):
-        assert user_david.is_active
-
-    with freeze_time(datetime.utcnow() + timedelta(minutes=31)):
+class TestUserActivity:
+    def test_user_activity_default(self, users_repository, user_david):
         assert not user_david.is_active
 
+    def test_user_activity_in_use(self, app, user_david, dossier_plfss2018):
+        app.get("/dossiers/plfss-2018/", user=user_david)
+        assert user_david.is_active
 
-def test_user_set_team(user_david):
-    from zam_repondeur.models import DBSession, User, Team
+    def test_user_activity_not_anymore_in_use(self, app, user_david, dossier_plfss2018):
+        app.get("/dossiers/plfss-2018/", user=user_david)
+        assert user_david.is_active
 
-    team_zam = DBSession.query(Team).first()
-    assert DBSession.query(User).first().teams == [team_zam]
+        with freeze_time(datetime.utcnow() + timedelta(minutes=29)):
+            assert user_david.is_active
+
+        with freeze_time(datetime.utcnow() + timedelta(minutes=31)):
+            assert not user_david.is_active
 
 
-def test_user_unset_team(user_david):
-    from zam_repondeur.models import DBSession, User, Team
+class TestUserTeamRelationship:
+    def test_user_set_team(self, user_david):
+        from zam_repondeur.models import DBSession, User, Team
 
-    user_david = DBSession.query(User).first()
-    team_zam = DBSession.query(Team).first()
-    user_david.teams.remove(team_zam)
-    assert DBSession.query(User).first().teams == []
+        team_zam = DBSession.query(Team).first()
+        assert DBSession.query(User).first().teams == [team_zam]
+
+    def test_user_unset_team(self, user_david):
+        from zam_repondeur.models import DBSession, User, Team
+
+        user_david = DBSession.query(User).first()
+        team_zam = DBSession.query(Team).first()
+        user_david.teams.remove(team_zam)
+        assert DBSession.query(User).first().teams == []

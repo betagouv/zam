7cf5427f8289d561a245caf7192c614afe7d0ef6
Ronan Amicel <ronan.amicel@gmail.com>
2019-09-04 14:48:51 +0200

    Allow *@*.gouv.fr addresses in Docker development environment

--- repondeur/docker/start-webapp.sh
+++ repondeur/docker/start-webapp.sh
@@ -2,4 +2,5 @@
 set -o errexit
 alembic --config=development-docker.ini upgrade head
 zam_load_data development-docker.ini
+zam_whitelist development-docker.ini add *@*.gouv.fr || true
 pserve --reload development-docker.ini app=zam_webapp

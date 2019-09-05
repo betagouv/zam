50d842008058308d7b4789387476bd4cb401fda7
David Larlet <david@larlet.fr>
2019-09-05 00:18:53 +0000

    Deal with magic paste and current selection/position

--- repondeur/zam_repondeur/static/js/dossiers.js
+++ repondeur/zam_repondeur/static/js/dossiers.js
@@ -20,7 +20,8 @@ application.register(
   'dossier-invite',
   class extends Stimulus.Controller {
     clean(event) {
-      const initial = event.target.value.trim()
+      const textarea = event.target
+      const initial = textarea.value.trim()
 
       /* https://stackoverflow.com/a/52078216 */
       const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
@@ -37,7 +38,33 @@ application.register(
       if (!emails.length) {
         emails = [paste]
       }
-      event.target.value = `${initial}\n${emails.join('\n')}`.trim()
+
+      // Flatten and trim emails.
+      emails = emails.reduce((a, b) => a.concat(b), [])
+      const result = emails.map(email => email.trim()).join('\n')
+
+      // Deal with cursor not at the end (and potentially selected text).
+      const selectedText = textarea.value.substring(
+        textarea.selectionStart,
+        textarea.selectionEnd
+      )
+      if (!selectedText) {
+        textarea.value =
+          textarea.value.substring(0, textarea.selectionStart) +
+          '\n' +
+          result +
+          '\n' +
+          textarea.value.substring(textarea.selectionEnd, textarea.value.length)
+      } else {
+        textarea.value = textarea.value.replace(selectedText, result)
+      }
+
+      // Remove remaining empty lines.
+      textarea.value = textarea.value
+        .split('\n')
+        .filter(line => line.trim())
+        .join('\n')
+
       event.preventDefault()
     }
   }
@@ -59,4 +86,3 @@ application.register(
     }
   }
 )
-

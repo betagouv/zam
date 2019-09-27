betagouv/zam

commit 5d82bc0b42fd8fd942adab71fab3291cce4be154

Set a delimiter between amdts in derouleur or not

repondeur/tests/integration/test_amendements_filtering.py
@@ -58,7 +58,7 @@ def test_column_filtering_by_value(
         user_ronan_table_an.amendements.append(amendements_an[0])
         user_david_table_an.amendements.append(amendements_an[1])
         amendement = Amendement.create(
-            lecture=lecture_an, article=article7bis_an, num=777
+            lecture=lecture_an, article=article7bis_an, num=777, position=3
         )
         user_daniel_table_an.amendements.append(amendement)
 
@@ -198,7 +198,7 @@ def test_column_filtering_by_value_with_batches(
         user_ronan_table_an.amendements.append(amendements_an[0])
         user_david_table_an.amendements.append(amendements_an[1])
         amendement = Amendement.create(
-            lecture=lecture_an, article=article7bis_an, num=777
+            lecture=lecture_an, article=article7bis_an, num=777, position=3
         )
         user_daniel_table_an.amendements.append(amendement)
 
@@ -260,6 +260,7 @@ def test_column_filtering_by_checkbox(
             lecture=lecture_an,
             article=article7bis_an,
             num=777,
+            position=3,
             auteur="LE GOUVERNEMENT",
         )
         user_david_table_an.amendements.append(amendement)
@@ -349,6 +350,7 @@ def test_column_filtering_by_value_for_missions(
             lecture=lecture_plf2018_an_premiere_lecture_seance_publique_2,
             article=article7bis_an,
             num=222,
+            position=3,
             mission=mission,
         )
         DBSession.add(amendement)

repondeur/tests/test_amendements.py
@@ -62,6 +62,10 @@ def test_get_amendements_order_default(app, lecture_an_url, amendements_an, user
         "666",
         "999",
     ]
+    headers_rows_length = 3
+    assert [" ".join(node.text().strip().split()) for node in resp.parser.css("tr")][
+        headers_rows_length:
+    ] == ["Art. 1 666 Voir", "Art. 1 999 Voir"]
 
 
 def test_get_amendements_order_fallback_article(
@@ -90,6 +94,7 @@ def test_get_amendements_order_abandoned_last(
     from zam_repondeur.models import DBSession
 
     with transaction.manager:
+        amendements_an[0].position = None
         amendements_an[0].sort = "Irrecevable"
         for amendement in amendements_an:
             amendement.user_content.avis = "Favorable"
@@ -99,10 +104,45 @@ def test_get_amendements_order_abandoned_last(
 
     assert resp.status_code == 200
     assert "Dossier de banc" in resp.text
-    assert [
-        " ".join(node.text().strip().split())
-        for node in resp.parser.css("tr td:nth-child(3)")
-    ] == ["999", "666 Irr."]
+    headers_rows_length = 3
+    assert [" ".join(node.text().strip().split()) for node in resp.parser.css("tr")][
+        headers_rows_length:
+    ] == [
+        "Art. 1 999 Voir",
+        (
+            "Les amendements en-deçà de cette ligne ne sont pas (encore) présents "
+            "dans le dérouleur."
+        ),
+        "Art. 1 666 Irr. Voir",
+    ]
+
+
+def test_get_amendements_order_with_missing_position(
+    app, lecture_an_url, amendements_an, user_david
+):
+    from zam_repondeur.models import DBSession
+
+    with transaction.manager:
+        amendements_an[0].position = None
+        for amendement in amendements_an:
+            amendement.user_content.avis = "Favorable"
+        DBSession.add_all(amendements_an)
+
+    resp = app.get(f"{lecture_an_url}/amendements/", user=user_david)
+
+    assert resp.status_code == 200
+    assert "Dossier de banc" in resp.text
+    headers_rows_length = 3
+    assert [" ".join(node.text().strip().split()) for node in resp.parser.css("tr")][
+        headers_rows_length:
+    ] == [
+        "Art. 1 999 Voir",
+        (
+            "Les amendements en-deçà de cette ligne ne sont pas (encore) présents "
+            "dans le dérouleur."
+        ),
+        "Art. 1 666 Voir",
+    ]
 
 
 def test_get_amendements_not_found_bad_format(app, user_david):

repondeur/zam_repondeur/static/css/amendements.css
@@ -150,3 +150,8 @@ span.avis.abandoned_before_seance {
 td form {
     display: inline-block;
 }
+tr.limit-derouleur {
+    text-align: center;
+    background-color: #ffc107;
+    font-style: italic;
+}

repondeur/zam_repondeur/templates/amendements.html
@@ -129,6 +129,23 @@
         <tbody data-controller="amendements-backlinks" data-target="amendements-filters.tbody">
         {% set highlighted_amdt = request.session.pop('highlighted_amdt', None) %}
         {% for amendement in collapsed_amendements | sort %}
+            {% if loop.previtem is defined and loop.previtem.position and not amendement.position %}
+                <tr class="limit-derouleur"
+                    {# Empty data attributes to avoid messing up with filters. #}
+                    data-article=""
+                    data-mission=""
+                    data-order=""
+                    data-amendement=""
+                    data-table=""
+                    data-emptytable=""
+                    data-gouvernemental=""
+                    data-abandoned-before-seance=""
+                    >
+                    <td colspan="{% if lecture.has_missions %}8{% else %}7{% endif %}">
+                        Les amendements en-deçà de cette ligne ne sont pas (encore) présents dans le dérouleur.
+                    </td>
+                </tr>
+            {% endif %}
             <tr data-article="
                     {%- if amendement.is_abandoned_before_seance -%}
                         {{ amendement.VERY_BIG_NUMBER }}

commit 932934a5845c45b1c4afc3be725a46bd4efeae91

Do not count derouleur limit as an amendement
Previously, the global number of amendements was incorrect with something as weird as 5 amendements (4 au total)

Bug introduced in https://github.com/betagouv/zam/pull/723

repondeur/zam_repondeur/static/js/amendements.js
@@ -236,7 +236,7 @@ class AmendementsFilters extends Stimulus.Controller {
   updateCount() {
     const initial = parseInt(this.data.get('initial-count'))
     const visibleRows = this.tbodyTarget.querySelectorAll(
-      'tr:not([class^=hidden])'
+      'tr:not([class^=hidden]):not([class=limit-derouleur])'
     )
     if (!visibleRows.length) {
       this.countTarget.innerHTML = `Aucun amendement (${initial} au total)`

repondeur/tests/integration/test_amendements_filtering.py
@@ -21,6 +21,36 @@ def test_filters_are_ineffective_without_amendements(
     assert not thead.find_element_by_css_selector("tr.filters").is_displayed()
 
 
+def test_number_of_amendements_is_displayed(
+    wsgi_server, driver, lecture_an_url, amendements_an
+):
+    driver.get(f"{lecture_an_url}/amendements/")
+    trs = driver.find_elements_by_css_selector("tbody tr")
+    assert len(trs) == 2
+    counter = driver.find_element_by_css_selector(
+        'span[data-target="amendements-filters.count"]'
+    )
+    assert counter.text == "2 amendements"
+
+
+def test_number_of_amendements_is_displayed_with_limit_derouleur(
+    wsgi_server, driver, lecture_an_url, amendements_an
+):
+    from zam_repondeur.models import DBSession
+
+    with transaction.manager:
+        amendements_an[0].position = None
+        DBSession.add(amendements_an[0])
+
+    driver.get(f"{lecture_an_url}/amendements/")
+    trs = driver.find_elements_by_css_selector("tbody tr")
+    assert len(trs) == 3
+    counter = driver.find_element_by_css_selector(
+        'span[data-target="amendements-filters.count"]'
+    )
+    assert counter.text == "2 amendements"
+
+
 @pytest.mark.parametrize(
     "column_index,input_text,kind,initial,filtered",
     [
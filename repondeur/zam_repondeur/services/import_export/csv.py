1c2b195c2b450a252e878201d3b8e8701c883c0e
Ronan Amicel <ronan.amicel@gmail.com>
2019-10-01 16:10:36 +0200

    Refactor: move _import_reponses_from_csv_file() to services package

--- repondeur/zam_repondeur/services/import_export/csv.py
+++ repondeur/zam_repondeur/services/import_export/csv.py
@@ -1,11 +1,19 @@
 import csv
+import io
 from collections import Counter
+from typing import BinaryIO, Dict, TextIO
 
 from pyramid.request import Request
 
-from zam_repondeur.models import Lecture
+from zam_repondeur.models import Amendement, Lecture, Team
 
-from .spreadsheet import FIELDS, HEADERS, export_amendement_for_spreadsheet
+from .common import import_amendement
+from .spreadsheet import (
+    FIELDS,
+    HEADERS,
+    column_name_to_field,
+    export_amendement_for_spreadsheet,
+)
 
 
 def export_csv(lecture: Lecture, filename: str, request: Request) -> Counter:
@@ -23,3 +31,55 @@ def export_csv(lecture: Lecture, filename: str, request: Request) -> Counter:
             writer.writerow(export_amendement_for_spreadsheet(amendement))
             counter["amendements"] += 1
     return counter
+
+
+class CSVError(Exception):
+    pass
+
+
+def _import_reponses_from_csv_file(
+    request: Request,
+    reponses_file: BinaryIO,
+    lecture: Lecture,
+    amendements: Dict[int, Amendement],
+    team: Team,
+) -> Counter:
+    previous_reponse = ""
+    counter = Counter({"reponses": 0, "reponses_errors": 0})
+
+    reponses_text_file = io.TextIOWrapper(reponses_file, encoding="utf-8-sig")
+
+    delimiter = _guess_csv_delimiter(reponses_text_file)
+
+    for line in csv.DictReader(reponses_text_file, delimiter=delimiter):
+        item = {
+            column_name_to_field(column_name): value
+            for column_name, value in line.items()
+            if column_name is not None
+        }
+        import_amendement(
+            request, lecture, amendements, item, counter, previous_reponse, team
+        )
+
+    return counter
+
+
+def _guess_csv_delimiter(text_file: TextIO) -> str:
+    try:
+        sample = text_file.readline()
+    except UnicodeDecodeError:
+        raise CSVError("Le fichier n’est pas encodé en UTF-8")
+    except Exception:
+        raise CSVError("Le format du fichier n’est pas reconnu")
+
+    try:
+        dialect = csv.Sniffer().sniff(sample, delimiters=",;\t")
+    except csv.Error:
+        raise CSVError(
+            "Le fichier CSV n’utilise pas un délimiteur reconnu "
+            "(virgule, point-virgule ou tabulation)"
+        )
+
+    text_file.seek(0)
+
+    return dialect.delimiter
--- repondeur/zam_repondeur/views/import_csv.py
+++ repondeur/zam_repondeur/views/import_csv.py
@@ -1,23 +1,15 @@
-import csv
-import io
-from collections import Counter
-from typing import BinaryIO, Dict, TextIO
-
 from pyramid.httpexceptions import HTTPFound
 from pyramid.request import Request
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from zam_repondeur.message import Message
-from zam_repondeur.models import Amendement, Lecture, Team
 from zam_repondeur.models.events.lecture import ReponsesImportees
 from zam_repondeur.resources import LectureResource
-from zam_repondeur.services.import_export.common import import_amendement
-from zam_repondeur.services.import_export.spreadsheet import column_name_to_field
-
-
-class CSVError(Exception):
-    pass
+from zam_repondeur.services.import_export.csv import (
+    CSVError,
+    _import_reponses_from_csv_file,
+)
 
 
 @view_config(context=LectureResource, name="import_csv", request_method="POST")
@@ -73,51 +65,3 @@ def import_csv(context: LectureResource, request: Request) -> Response:
         )
 
     return HTTPFound(location=next_url)
-
-
-def _import_reponses_from_csv_file(
-    request: Request,
-    reponses_file: BinaryIO,
-    lecture: Lecture,
-    amendements: Dict[int, Amendement],
-    team: Team,
-) -> Counter:
-    previous_reponse = ""
-    counter = Counter({"reponses": 0, "reponses_errors": 0})
-
-    reponses_text_file = io.TextIOWrapper(reponses_file, encoding="utf-8-sig")
-
-    delimiter = _guess_csv_delimiter(reponses_text_file)
-
-    for line in csv.DictReader(reponses_text_file, delimiter=delimiter):
-        item = {
-            column_name_to_field(column_name): value
-            for column_name, value in line.items()
-            if column_name is not None
-        }
-        import_amendement(
-            request, lecture, amendements, item, counter, previous_reponse, team
-        )
-
-    return counter
-
-
-def _guess_csv_delimiter(text_file: TextIO) -> str:
-    try:
-        sample = text_file.readline()
-    except UnicodeDecodeError:
-        raise CSVError("Le fichier n’est pas encodé en UTF-8")
-    except Exception:
-        raise CSVError("Le format du fichier n’est pas reconnu")
-
-    try:
-        dialect = csv.Sniffer().sniff(sample, delimiters=",;\t")
-    except csv.Error:
-        raise CSVError(
-            "Le fichier CSV n’utilise pas un délimiteur reconnu "
-            "(virgule, point-virgule ou tabulation)"
-        )
-
-    text_file.seek(0)
-
-    return dialect.delimiter

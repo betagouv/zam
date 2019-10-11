7144d22f16aee6db42f087f004f464c70232740e
Ronan Amicel <ronan.amicel@gmail.com>
2019-10-11 10:12:46 +0200

    Handle UTF-8 encoding in senateurs CSV
    Fixes: https://rollbar.com/zam/zam/items/377/
    Fixes: https://rollbar.com/zam/zam/items/378/
    Fixes: https://rollbar.com/zam/zam/items/379/

--- repondeur/zam_repondeur/services/fetch/senat/senateurs/__init__.py
+++ repondeur/zam_repondeur/services/fetch/senat/senateurs/__init__.py
@@ -6,6 +6,13 @@ from .parse import parse_senateurs
 
 
 def fetch_and_parse_senateurs() -> Dict[str, Senateur]:
-    lines = fetch_senateurs()
-    by_matricule = parse_senateurs(lines)  # type: Dict[str, Senateur]
-    return by_matricule
+    # 2019-10-11: encoding of the CSV file changed from CP1252 to UTF-8 without warning
+    data = fetch_senateurs()
+    for encoding in ["utf-8", "cp1252"]:
+        try:
+            lines = data.decode(encoding).splitlines()
+            by_matricule: Dict[str, Senateur] = parse_senateurs(lines)
+            return by_matricule
+        except (UnicodeDecodeError, KeyError):
+            continue
+    raise ValueError("Failed to parse sénateurs CSV")
--- repondeur/zam_repondeur/services/fetch/senat/senateurs/fetch.py
+++ repondeur/zam_repondeur/services/fetch/senat/senateurs/fetch.py
@@ -1,16 +1,12 @@
 from http import HTTPStatus
-from typing import List
 
 import requests
 
 URL = "https://data.senat.fr/data/senateurs/ODSEN_GENERAL.csv"
 
 
-def fetch_senateurs() -> List[str]:
+def fetch_senateurs() -> bytes:
     resp = requests.get(URL)
     if resp.status_code != HTTPStatus.OK:  # 200
         raise RuntimeError("Failed to download senateurs CSV file")
-
-    text = resp.content.decode("cp1252")
-    lines = text.splitlines()
-    return lines
+    return resp.content

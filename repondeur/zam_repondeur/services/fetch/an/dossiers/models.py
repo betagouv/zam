c6caab0c87fed48ccebab2eab14ebb4467a03ffe
David Larlet <david@larlet.fr>
2019-11-18 09:47:02 -0500

    Remove dead code

--- repondeur/zam_repondeur/services/fetch/an/dossiers/models.py
+++ repondeur/zam_repondeur/services/fetch/an/dossiers/models.py
@@ -96,16 +96,6 @@ class DossierRef:
     senat_url: str
     lectures: List[LectureRef]
 
-    @property
-    def most_recent_texte_date(self) -> Tuple[date, str]:
-        return (
-            max(
-                (lecture.texte.date_depot or MIN_DATE for lecture in self.lectures),
-                default=MIN_DATE,
-            ),
-            self.uid,
-        )
-
     @classmethod
     def merge_dossiers(
         cls, dossiers: DossierRefsByUID, others: DossierRefsByUID

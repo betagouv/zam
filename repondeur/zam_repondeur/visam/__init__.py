f34fa7036e53dfa0a5941264cef485ba34651e5d
Ronan Amicel <ronan.amicel@gmail.com>
2020-03-12 17:27:05 +0100

    Refactor: move custom Visam app name to includeme()

--- repondeur/zam_repondeur/visam/__init__.py
+++ repondeur/zam_repondeur/visam/__init__.py
@@ -5,6 +5,9 @@ from pyramid.config import Configurator
 
 
 def includeme(config: Configurator) -> None:
+    # Custom app name
+    config.add_settings({"zam.app_name": "Visam"})
+
     # Custom logo
     config.override_asset(
         to_override="zam_repondeur:static/",

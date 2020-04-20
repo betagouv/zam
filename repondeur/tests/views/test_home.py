20d82223a7b7a16635642606642f5a31d7ee7b29
Ronan Amicel <ronan.amicel@gmail.com>
2020-04-20 10:54:40 +0200

    Refactor: update test name

--- repondeur/tests/views/test_home.py
+++ repondeur/tests/views/test_home.py
@@ -1,4 +1,4 @@
-def test_home_redirects_to_lectures(app, user_david):
+def test_home_redirects_to_dossiers(app, user_david):
     resp = app.get("/", user=user_david)
     assert resp.status_code == 302
     assert resp.location == "https://zam.test/dossiers/"

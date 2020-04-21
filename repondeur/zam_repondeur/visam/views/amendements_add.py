83dce3c65eb45d7b87f3200ff96b22b66da7943f
Ronan Amicel <ronan.amicel@gmail.com>
2020-04-21 11:37:29 +0200

    Group of submitted amendement is the user's organisation for this conseil

--- repondeur/zam_repondeur/visam/views/amendements_add.py
+++ repondeur/zam_repondeur/visam/views/amendements_add.py
@@ -1,14 +1,16 @@
-from pyramid.httpexceptions import HTTPFound
+from typing import Optional
+
+from pyramid.httpexceptions import HTTPBadRequest, HTTPFound
 from pyramid.request import Request
 from pyramid.response import Response
 from pyramid.view import view_config, view_defaults
 
 from zam_repondeur.message import Message
-from zam_repondeur.models import Amendement, Article, DBSession
+from zam_repondeur.models import Amendement, Article, DBSession, Lecture
 from zam_repondeur.models.division import SubDiv
 from zam_repondeur.resources import AmendementCollection
 from zam_repondeur.services.clean import clean_html
-from zam_repondeur.visam.models import AmendementSaisi, Organisation
+from zam_repondeur.visam.models import AmendementSaisi, Organisation, UserMembership
 
 
 @view_defaults(context=AmendementCollection, name="saisie")
@@ -16,7 +18,10 @@ class AddAmendementView:
     def __init__(self, context: AmendementCollection, request: Request):
         self.context = context
         self.request = request
-        self.lecture = self.context.parent.model()
+        self.lecture: Lecture = self.context.parent.model()
+        self.membership: Optional[UserMembership] = self.request.user.membership_of(
+            self.lecture.chambre
+        )
 
     @view_config(request_method="GET", renderer="amendements_add.html")
     def get(self) -> dict:
@@ -42,8 +47,9 @@ class AddAmendementView:
         if self.can_select_organisation():
             organisation_name = self.request.POST.get("organisation")
         else:
-            # WARNING: what if the user is part of two organisations?
-            organisation_name = self.request.user.organisations[0].name
+            if self.membership is None:
+                raise HTTPBadRequest
+            organisation_name = self.membership.organisation.name
 
         num = self.next_num_for(organisation_name)
         corps = clean_html(self.request.POST.get("corps", ""))
@@ -109,7 +115,6 @@ class AddAmendementView:
         return groupe_title + " " + str(max_num + 1)
 
     def can_select_organisation(self) -> bool:
-        return any(
-            organisation.is_gouvernement
-            for organisation in self.request.user.organisations
+        return (
+            self.membership is not None and self.membership.organisation.is_gouvernement
         )
